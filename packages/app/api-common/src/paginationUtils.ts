01a442af9cac6699014d82a17a88ce290320f839
Anton Malich <anton.malich@lokalise.com>
2023-11-23 14:15:02 +0300

    + getPaginatedEntries (#5)
    * + getPaginatedEntries

--- packages/app/api-common/src/paginationUtils.ts
+++ packages/app/api-common/src/paginationUtils.ts
@@ -1,6 +1,28 @@
+import type { PaginationMeta, PaginationParams } from './apiSchemas'
+
 export function getMetaFor<T extends { id: string }>(data: T[]) {
 	return {
 		count: data.length,
 		cursor: data.length > 0 ? data[data.length - 1].id : undefined,
 	}
+}
+
+type PaginatedResponse<T> = {
+	data: T[]
+	meta: PaginationMeta
+}
+
+export async function getPaginatedEntries<T>(
+	pagination: PaginationParams,
+	apiCall: (params: PaginationParams) => Promise<PaginatedResponse<T>>,
+): Promise<T[]> {
+	const resultArray: T[] = []
+	let currentCursor: string | undefined = undefined
+	do {
+		const pageResult = await apiCall({ ...pagination, after: currentCursor })
+		resultArray.push(...pageResult.data)
+		currentCursor = pageResult.meta.cursor
+	} while (currentCursor)
+
+	return resultArray
 }
