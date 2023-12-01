742bcac84a1d4f21c49d9f82cf750712baebb7c8
CarlosGamero <carlos.gamero@lokalise.com>
2023-12-01 10:41:21 +0100

    Renaming paginationParams to OptionalPaginationParams

--- packages/app/api-common/src/apiSchemas.ts
+++ packages/app/api-common/src/apiSchemas.ts
@@ -1,13 +1,13 @@
 import z from 'zod'
 
-export const PAGINATION_CONFIG_SCHEMA = z.object({
+export const OPTIONAL_PAGINATION_CONFIG_SCHEMA = z.object({
 	limit: z.number().gt(0).optional(),
 	before: z.string().min(1).optional(),
 	after: z.string().min(1).optional(),
 })
-export type PaginationParams = z.infer<typeof PAGINATION_CONFIG_SCHEMA>
+export type OptionalPaginationParams = z.infer<typeof OPTIONAL_PAGINATION_CONFIG_SCHEMA>
 
-export const MANDATORY_PAGINATION_CONFIG_SCHEMA = PAGINATION_CONFIG_SCHEMA.extend({
+export const MANDATORY_PAGINATION_CONFIG_SCHEMA = OPTIONAL_PAGINATION_CONFIG_SCHEMA.extend({
 	limit: z.number().gt(0),
 })
 export type MandatoryPaginationParams = z.infer<typeof MANDATORY_PAGINATION_CONFIG_SCHEMA>
