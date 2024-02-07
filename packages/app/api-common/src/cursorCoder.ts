7d903896e9785a56e04713e8fb7199f07176f9eb
CarlosGamero <carlos.gamero@lokalise.com>
2024-02-07 14:11:54 +0100

    AP-2181 Minor change

--- packages/app/api-common/src/cursorCoder.ts
+++ packages/app/api-common/src/cursorCoder.ts
@@ -1,9 +1,9 @@
 import { isObject } from './typeUtils'
 
-export const encodeCursor = (object: object): string =>
+export const encodeCursor = (object: Record<string, unknown>): string =>
 	Buffer.from(JSON.stringify(object)).toString('base64url')
 
-export const decodeCursor = (value: string): object => {
+export const decodeCursor = (value: string): Record<string, unknown> => {
 	const decoded = Buffer.from(value, 'base64url').toString('utf-8')
 	const result: unknown = JSON.parse(decoded)
 	if (result && isObject(result)) {
