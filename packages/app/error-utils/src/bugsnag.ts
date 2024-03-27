b43ce866b6c92d48972875b79295f934ff8e172f
Bartosz Drozd <bartosz.drozd@lokalise.com>
2024-03-27 14:00:09 +0100

    feat(AP-2892): ensure Bugsnag is not started before starting it

--- packages/app/error-utils/src/bugsnag.ts
+++ packages/app/error-utils/src/bugsnag.ts
@@ -1,7 +1,7 @@
 import type { Event, NotifiableError } from '@bugsnag/js'
 import Bugsnag from '@bugsnag/js'
-import type { ErrorReporter } from '@lokalise/node-core'
 import type { NodeConfig } from '@bugsnag/node'
+import type { ErrorReporter } from '@lokalise/node-core'
 
 export type Severity = Event['severity']
 
@@ -27,7 +27,11 @@ export const reportErrorToBugsnag = ({
 		}
 	})
 
-export const startBugsnag = (config: NodeConfig) => Bugsnag.start(config)
+export const startBugsnag = (config: NodeConfig) => {
+	if (!Bugsnag.isStarted()) {
+		Bugsnag.start(config)
+	}
+}
 
 export const bugsnagErrorReporter: ErrorReporter = {
 	report: (report) => reportErrorToBugsnag(report),
