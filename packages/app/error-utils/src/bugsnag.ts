924b394e5bc0a87a49bcd319781eb7aa2c5f9e56
Bartosz Drozd <bartosz.drozd@lokalise.com>
2024-03-27 11:23:15 +0100

    feat(AP-2892): introduce error-utils

---
+++ packages/app/error-utils/src/bugsnag.ts
@@ -0,0 +1,31 @@
+import type { Event, NotifiableError } from '@bugsnag/js'
+import Bugsnag from '@bugsnag/js'
+import type { ErrorReporter } from '@lokalise/node-core'
+
+export type Severity = Event['severity']
+
+export interface ErrorReport {
+	error: NotifiableError
+	severity?: Severity
+	unhandled?: boolean
+	context?: Record<string, unknown>
+}
+
+export const reportErrorToBugsnag = ({
+	error,
+	severity = 'error',
+	unhandled = true,
+	context,
+}: ErrorReport) =>
+	Bugsnag.isStarted() &&
+	Bugsnag.notify(error, (event) => {
+		event.severity = severity
+		event.unhandled = unhandled
+		if (context) {
+			event.addMetadata('Context', context)
+		}
+	})
+
+export const bugsnagErrorReporter: ErrorReporter = {
+	report: (report) => reportErrorToBugsnag(report),
+}
