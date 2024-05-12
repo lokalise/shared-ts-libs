7e1f277421351781b006819b3d1c319660a3ebb9
Igor Savin <iselwin@gmail.com>
2024-05-12 08:45:58 +0300

    Extract backend-http-client from node-core (#145)

---
+++ packages/app/backend-http-client/index.ts
@@ -0,0 +1,16 @@
+export {
+	sendPut,
+	sendPutBinary,
+	sendDelete,
+	sendPatch,
+	sendGet,
+	sendPost,
+	sendPostBinary,
+	httpClient,
+	buildClient,
+	type RequestOptions,
+	type Response,
+	type HttpRequestContext,
+	type ResponseSchema,
+	JSON_HEADERS,
+} from './src/httpClient'
