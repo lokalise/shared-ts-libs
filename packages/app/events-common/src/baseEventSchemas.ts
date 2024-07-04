379e0f8090f52cc835844b161be9c42fc35fcd3c
Igor Savin <iselwin@gmail.com>
2024-07-04 18:29:31 +0300

    Accept any string as a correlationId (#205)

--- packages/app/events-common/src/baseEventSchemas.ts
+++ packages/app/events-common/src/baseEventSchemas.ts
@@ -5,19 +5,16 @@ export const BASE_EVENT_SCHEMA = z.object({
 	id: z.string().uuid().describe('event unique identifier'),
 	type: z.literal<string>('<replace.me>').describe('event type name'),
 	timestamp: z.string().datetime().describe('iso 8601 datetime'),
-	source: z.string().nonempty().describe('source service of the event'),
+	source: z.string().min(1).describe('source service of the event'),
 	payload: z.optional(z.object({})).describe('event payload based on type'),
 	metadata: z
 		.object({
-			schemaVersion: z.string().nonempty().describe('base event schema version'),
-			originalApp: z.string().nonempty().describe('app/service initiated workflow'),
+			schemaVersion: z.string().min(1).describe('base event schema version'),
+			originalApp: z.string().min(1).describe('app/service initiated workflow'),
 		})
 		.describe('event metadata'),
-	correlationId: z
-		.string()
-		.uuid()
-		.describe('unique identifier passed to all events in workflow chain'),
-	version: z.string().nonempty().describe('event payload version'),
+	correlationId: z.string().describe('unique identifier passed to all events in workflow chain'),
+	version: z.string().min(1).describe('event payload version'),
 })
 
 export type BaseEventType = z.infer<typeof BASE_EVENT_SCHEMA>
