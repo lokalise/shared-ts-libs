5873040573cf64d3d9702c29fa9d2488ec467600
Igor Savin <iselwin@gmail.com>
2024-07-09 12:40:36 +0300

    Switch backend client to biome (#209)

--- packages/app/backend-http-client/src/errors/ResponseStatusError.ts
+++ packages/app/backend-http-client/src/errors/ResponseStatusError.ts
@@ -2,28 +2,28 @@ import { InternalError } from '@lokalise/node-core'
 import type { RequestResult } from 'undici-retry'
 
 export function isResponseStatusError(entity: unknown): entity is ResponseStatusError {
-	return 'isResponseStatusError' in (entity as ResponseStatusError)
+  return 'isResponseStatusError' in (entity as ResponseStatusError)
 }
 
 export class ResponseStatusError extends InternalError {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	public readonly response: RequestResult<any>
-	public readonly isResponseStatusError = true
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  public readonly response: RequestResult<any>
+  public readonly isResponseStatusError = true
 
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	constructor(requestResult: RequestResult<any>, requestLabel = 'N/A') {
-		super({
-			message: `Response status code ${requestResult.statusCode}`,
-			details: {
-				requestLabel,
-				response: {
-					statusCode: requestResult.statusCode,
-					body: requestResult.body,
-				},
-			},
-			errorCode: 'REQUEST_ERROR',
-		})
-		this.response = requestResult
-		this.name = 'ResponseStatusError'
-	}
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  constructor(requestResult: RequestResult<any>, requestLabel = 'N/A') {
+    super({
+      message: `Response status code ${requestResult.statusCode}`,
+      details: {
+        requestLabel,
+        response: {
+          statusCode: requestResult.statusCode,
+          body: requestResult.body,
+        },
+      },
+      errorCode: 'REQUEST_ERROR',
+    })
+    this.response = requestResult
+    this.name = 'ResponseStatusError'
+  }
 }
