92cab2e001449499846111e7448e75372f75bd22
CarlosGamero <carlos.gamero@lokalise.com>
2024-07-15 14:55:35 +0200

    using biome.package config

--- packages/app/prisma-utils/src/index.ts
+++ packages/app/prisma-utils/src/index.ts
@@ -1,6 +1,5 @@
 export type * from './types'
 
-// biome-ignore lint/performance/noBarrelFile: <explanation>
 export * from './prismaError'
 
 export { prismaTransaction } from './prismaTransaction'
