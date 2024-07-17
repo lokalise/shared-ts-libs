f434d25156ebc2fd0133b6c415ce10d7c8b5d3fa
Ondrej Sevcik <ondrej.sevcik@lokalise.com>
2024-07-17 13:39:16 +0200

    feat: reformat with biome

--- packages/app/universal-ts-utils/src/array/uniq.spec.ts
+++ packages/app/universal-ts-utils/src/array/uniq.spec.ts
@@ -2,11 +2,20 @@ import { describe, expect, it } from 'vitest'
 import { uniq } from './uniq'
 
 describe('uniq', () => {
-	it('returns a new array of mixed primitive value without duplicates', () => {
-		const objectA = {};
-		const values = [1, 'a', Number.NaN, true, false, objectA, null, undefined];
-		const duplicateValues =  [...values, ...values];
+  it('returns a new array of mixed primitive value without duplicates', () => {
+    const objectA = {}
+    const values = [1, 'a', Number.NaN, true, false, objectA, null, undefined]
+    const duplicateValues = [...values, ...values]
 
-		expect(uniq(duplicateValues)).toEqual([1, 'a', Number.NaN, true, false, objectA, null, undefined])
-	})
+    expect(uniq(duplicateValues)).toEqual([
+      1,
+      'a',
+      Number.NaN,
+      true,
+      false,
+      objectA,
+      null,
+      undefined,
+    ])
+  })
 })
