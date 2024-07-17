9f1264e118aa997805e849f27828442cf8fb5f61
Ondrej Sevcik <ondrej.sevcik@lokalise.com>
2024-07-17 14:16:00 +0200

    feat: add duplicate values

--- packages/app/universal-ts-utils/src/array/uniq.spec.ts
+++ packages/app/universal-ts-utils/src/array/uniq.spec.ts
@@ -4,8 +4,27 @@ import { uniq } from './uniq'
 describe('uniq', () => {
   it('returns a new array of mixed primitive value without duplicates', () => {
     const objectA = {}
-    const values = [1, 'a', Number.NaN, true, false, objectA, null, undefined]
-    const duplicateValues = [...values, ...values]
+    const objectB = {}
+    const duplicateValues = [
+      1,
+      1,
+      'a',
+      'a',
+      Number.NaN,
+      Number.NaN,
+      true,
+      true,
+      false,
+      false,
+      null,
+      null,
+      undefined,
+      undefined,
+      objectA,
+      objectA,
+      objectB,
+      objectB,
+    ]
 
     expect(uniq(duplicateValues)).toEqual([
       1,
@@ -13,9 +32,10 @@ describe('uniq', () => {
       Number.NaN,
       true,
       false,
-      objectA,
       null,
       undefined,
+      objectA,
+      objectB,
     ])
   })
 })
