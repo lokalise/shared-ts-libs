c3f6a1365db11d20e4859b1d4fc25a3a55f2c69d
Ondrej Sevcik <ondrej.sevcik@lokalise.com>
2024-07-18 10:36:31 +0200

    adjust comment

--- packages/app/universal-ts-utils/src/array/unique.ts
+++ packages/app/universal-ts-utils/src/array/unique.ts
@@ -1,5 +1,6 @@
 /**
  * A method to remove duplicates from an array of primitive values.
- * Elements are compared by reference using a Set.
+ * Value equality is based on the SameValueZero algorithm.
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set#value_equality
  */
 export const unique = <T>(arr: T[]): T[] => Array.from(new Set(arr))
