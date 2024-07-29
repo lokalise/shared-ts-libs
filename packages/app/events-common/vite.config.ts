491648a0b991d5a87ef085702f9ba0bb7f5944eb
CarlosGamero <101278162+CarlosGamero@users.noreply.github.com>
2024-07-29 21:08:31 +0200

    vitest update (#254)

--- packages/app/events-common/vite.config.ts
+++ packages/app/events-common/vite.config.ts
@@ -6,17 +6,17 @@ import packageJson from './package.json'
 
 /* eslint-disable import/no-default-export */
 export default defineConfig({
-	entry: resolve(__dirname, 'src/index.ts'),
-	dependencies: Object.keys(packageJson.peerDependencies),
-	test: {
-		coverage: {
-			exclude: ['src/**/*.spec.ts', 'src/**/*.test.ts', 'src/**/*Types.ts', 'src/**/*types.ts'],
-			thresholds: {
-				lines: 96,
-				functions: 0, // not applicable
-				branches: 0, // not applicable
-				statements: 96,
-			},
-		},
-	},
+  entry: resolve(__dirname, 'src/index.ts'),
+  dependencies: Object.keys(packageJson.peerDependencies),
+  test: {
+    coverage: {
+      exclude: ['src/**/index.ts', 'src/**/*.spec.ts', 'src/**/*.test.ts', 'src/**/*types.ts'],
+      thresholds: {
+        lines: 96,
+        functions: 0, // not applicable
+        branches: 0, // not applicable
+        statements: 96,
+      },
+    },
+  },
 })
