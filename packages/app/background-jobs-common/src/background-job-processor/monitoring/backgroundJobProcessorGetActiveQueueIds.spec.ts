ea3044693803a28c5022a8e70d8a01da38fde8a6
CarlosGamero <carlos.gamero@lokalise.com>
2024-08-02 18:24:14 +0200

    backgroundJobProcessorGetActiveQueueIds tests

---
+++ packages/app/background-jobs-common/src/background-job-processor/monitoring/backgroundJobProcessorGetActiveQueueIds.spec.ts
@@ -0,0 +1,44 @@
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+
+import type Redis from 'ioredis'
+import { DependencyMocks } from '../../../test/dependencyMocks'
+import { QUEUE_IDS_KEY, RETENTION_QUEUE_IDS_IN_DAYS } from '../constants'
+import { daysToMilliseconds } from '../utils'
+import { backgroundJobProcessorGetActiveQueueIds } from './backgroundJobProcessorGetActiveQueueIds'
+
+describe('backgroundJobProcessorGetActiveQueueIds', () => {
+  let mocks: DependencyMocks
+  //let deps: BackgroundJobProcessorDependencies<any>
+  let redis: Redis
+
+  beforeAll(() => {
+    mocks = new DependencyMocks()
+    //deps = mocks.create()
+    redis = mocks.startRedis()
+  })
+
+  beforeEach(async () => {
+    await redis?.flushall('SYNC')
+  })
+
+  afterAll(async () => {
+    redis.disconnect()
+    await mocks.dispose()
+  })
+
+  it.each([[true], [false]])(
+    'returns not expired elements on the set using redis client=%s',
+    async (useRedisClient) => {
+      const retentionMs = daysToMilliseconds(RETENTION_QUEUE_IDS_IN_DAYS)
+
+      await redis.zadd(QUEUE_IDS_KEY, Date.now() - retentionMs, 'expired')
+      await redis.zadd(QUEUE_IDS_KEY, Date.now(), 'queue2')
+      await redis.zadd(QUEUE_IDS_KEY, Date.now() - retentionMs + 100, 'queue1')
+
+      const queues = await backgroundJobProcessorGetActiveQueueIds(
+        useRedisClient ? redis : mocks.getRedisConfig(),
+      )
+      expect(queues).toEqual(['queue1', 'queue2'])
+    },
+  )
+})
