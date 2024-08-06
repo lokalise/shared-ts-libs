3936c27c6355232d71b6cc9fd2a0692cfbcf2551
CarlosGamero <carlos.gamero@lokalise.com>
2024-08-06 15:57:18 +0200

    Minor refactor on NTC constants

--- packages/app/non-translatable-markup/src/index.ts
+++ packages/app/non-translatable-markup/src/index.ts
@@ -1,1 +1,2 @@
+export * from './nonTranslatableTags'
 export * from './nonTranslatableContentHelper'
--- packages/app/non-translatable-markup/src/nonTranslatableContentHelper.ts
+++ packages/app/non-translatable-markup/src/nonTranslatableContentHelper.ts
@@ -1,21 +1,16 @@
-export const NON_TRANSLATABLE_START_TAG = '\uE101'
-export const NON_TRANSLATABLE_END_TAG = '\uE102'
-
-const nonTranslatableTextPattern = `${NON_TRANSLATABLE_START_TAG}.*?${NON_TRANSLATABLE_END_TAG}`
-const nonTranslatableTextRegexp = new RegExp(nonTranslatableTextPattern)
-const nonTranslatableTextRegexpG = new RegExp(nonTranslatableTextPattern, 'g')
-const nonTranslatableTagsRegexpG = new RegExp(
-	`[${NON_TRANSLATABLE_START_TAG}${NON_TRANSLATABLE_END_TAG}]`,
-	'g',
-)
+import {
+  nonTranslatableTagsRegexpG,
+  nonTranslatableTextRegexp,
+  nonTranslatableTextRegexpG,
+} from './regex'
 
 /**
  * Returns true if the text is entirely encapsulated in non-translatable tags.
  */
 export const isTextTranslatable = (text: string): boolean => {
-	const parts = text.split(nonTranslatableTextRegexp).map((part) => part.trim())
+  const parts = text.split(nonTranslatableTextRegexp).map((part) => part.trim())
 
-	return parts.some((part) => part !== '')
+  return parts.some((part) => part !== '')
 }
 
 /**
@@ -23,46 +18,46 @@ export const isTextTranslatable = (text: string): boolean => {
  * the original string, or add/remove one or more non-translatable tags.
  */
 export const isAttemptToEditNonTranslatableContent = (
-	text: string,
-	updatedText: string,
+  text: string,
+  updatedText: string,
 ): boolean => {
-	if (!isTextTranslatable(updatedText)) {
-		return true
-	}
+  if (!isTextTranslatable(updatedText)) {
+    return true
+  }
 
-	const nonTranslatableContentInText = extractTextBetweenTags(text)
-	const nonTranslatableContentInUpdatedText = extractTextBetweenTags(updatedText)
+  const nonTranslatableContentInText = extractTextBetweenTags(text)
+  const nonTranslatableContentInUpdatedText = extractTextBetweenTags(updatedText)
 
-	if (nonTranslatableContentInText.length !== nonTranslatableContentInUpdatedText.length) {
-		return true
-	}
+  if (nonTranslatableContentInText.length !== nonTranslatableContentInUpdatedText.length) {
+    return true
+  }
 
-	const sortedNonTranslatableContentInText = nonTranslatableContentInText.sort()
-	const sortedNonTranslatableContentInUpdatedText = nonTranslatableContentInUpdatedText.sort()
+  const sortedNonTranslatableContentInText = nonTranslatableContentInText.sort()
+  const sortedNonTranslatableContentInUpdatedText = nonTranslatableContentInUpdatedText.sort()
 
-	for (let i = 0; i < sortedNonTranslatableContentInText.length; i++) {
-		if (sortedNonTranslatableContentInText[i] !== sortedNonTranslatableContentInUpdatedText[i]) {
-			return true
-		}
-	}
+  for (let i = 0; i < sortedNonTranslatableContentInText.length; i++) {
+    if (sortedNonTranslatableContentInText[i] !== sortedNonTranslatableContentInUpdatedText[i]) {
+      return true
+    }
+  }
 
-	return false
+  return false
 }
 
 const extractTextBetweenTags = (text: string): string[] => {
-	const matches = []
-	let match
+  const matches = []
+  let match
 
-	while ((match = nonTranslatableTextRegexpG.exec(text)) !== null) {
-		matches.push(match[0])
-	}
+  while ((match = nonTranslatableTextRegexpG.exec(text)) !== null) {
+    matches.push(match[0])
+  }
 
-	return matches
+  return matches
 }
 
 /**
  * Removes any non-translatable tag.
  */
 export const removeNonTranslatableTags = (text: string): string => {
-	return text.replace(nonTranslatableTagsRegexpG, '')
+  return text.replace(nonTranslatableTagsRegexpG, '')
 }
