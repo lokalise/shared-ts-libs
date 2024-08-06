3c142310c636ca9740edf6e384b472853e8d19c7
CarlosGamero <101278162+CarlosGamero@users.noreply.github.com>
2024-08-06 17:25:58 +0200

    NTC-biome (#261)
    * non-translatable-markup biome
    
    * Fixing biome issues
    
    * biome fixes
    
    * Removing unused dep
    
    * NTC using latest lokalise/biome-config
    
    * lint fix

--- packages/app/non-translatable-markup/vite.config.ts
+++ packages/app/non-translatable-markup/vite.config.ts
@@ -2,20 +2,22 @@ import { resolve } from 'node:path'
 
 import defineConfig from '@lokalise/package-vite-config/package'
 
+// @ts-ignore
 import packageJson from './package.json'
 
-/* eslint-disable import/no-default-export */
+// biome-ignore lint/style/noDefaultExport: <explanation>
 export default defineConfig({
-	entry: resolve(__dirname, 'src/index.ts'),
-	dependencies: Object.keys(packageJson.peerDependencies),
-	test: {
-		coverage: {
-			thresholds: {
-				lines: 100,
-				functions: 100,
-				branches: 100,
-				statements: 100,
-			},
-		},
-	},
+  entry: resolve(__dirname, 'src/index.ts'),
+  dependencies: Object.keys(packageJson.peerDependencies),
+  test: {
+    coverage: {
+      provider: 'v8',
+      thresholds: {
+        lines: 100,
+        functions: 100,
+        branches: 100,
+        statements: 100,
+      },
+    },
+  },
 })
