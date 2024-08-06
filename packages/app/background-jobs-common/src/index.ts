9b368b4578685bc1fbc8b8494323dac41598806c
Igor Savin <iselwin@gmail.com>
2024-08-06 21:45:19 +0300

    Add AbstractPeriodicJob to the lib

--- packages/app/background-jobs-common/src/index.ts
+++ packages/app/background-jobs-common/src/index.ts
@@ -2,3 +2,10 @@ import type { TransactionObservabilityManager } from '@lokalise/node-core'
 
 export type { TransactionObservabilityManager }
 export * from './background-job-processor'
+export {
+  AbstractPeriodicJob,
+  createTask,
+  type PeriodicJobDependencies,
+  type BackgroundJobConfiguration,
+  type LockConfiguration,
+} from './periodic-jobs/AbstractPeriodicJob'
