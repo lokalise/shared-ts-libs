a0bb248da003f51c3a1a2509c39df7f651fcac66
CarlosGamero <carlos.gamero@lokalise.com>
2024-08-07 15:40:04 +0200

    AP-4782 Improving regex

--- packages/app/non-translatable-markup/src/methods/utils.ts
+++ packages/app/non-translatable-markup/src/methods/utils.ts
@@ -1,10 +1,22 @@
 import { NON_TRANSLATABLE_END_TAG, NON_TRANSLATABLE_START_TAG } from '../nonTranslatableTags'
 
 const nonTranslatableTextPattern = `${NON_TRANSLATABLE_START_TAG}.*?${NON_TRANSLATABLE_END_TAG}`
-const nonTranslatableTagsPattern = `[${NON_TRANSLATABLE_START_TAG}${NON_TRANSLATABLE_END_TAG}]`
-const symbolsPattern = /[¡!¿?@#$%^&*/\\|"'`´)(\]\[}{><+=.,_-]/
-
 export const nonTranslatableTextRegexp = new RegExp(nonTranslatableTextPattern)
 export const nonTranslatableTextRegexpG = new RegExp(nonTranslatableTextPattern, 'g')
+
+const nonTranslatableTagsPattern = `[${NON_TRANSLATABLE_START_TAG}${NON_TRANSLATABLE_END_TAG}]`
 export const nonTranslatableTagsRegexpG = new RegExp(nonTranslatableTagsPattern, 'g')
+
+/**
+ * Explanation of the pattern:
+ *  `[...]` -> matches any one of the characters or Unicode property escapes specified within it
+ *      `\p{P}` -> matches any kind of punctuation character
+ *      `\p{S}` -> matches any kind of symbol character
+ *      `\p{So}` -> matches any other symbol character
+ *      `\p{Emoji}` -> matches any emoji character
+ *      `\p{Math}` -> matches any math symbol character
+ *  `u` -> enables unicode mode
+ */
+const symbolsPattern = /[\p{P}\p{S}\p{So}\p{Emoji}\p{Math}']/u
+
 export const symbolsRegexp = new RegExp(symbolsPattern)
