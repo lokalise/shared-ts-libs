475e9a4adce220c41c2f2cd7e270e3d9b78d19fa
CarlosGamero <carlos.gamero@lokalise.com>
2024-08-07 15:53:23 +0200

    AP-4762 Removing unused part

--- packages/app/non-translatable-markup/src/methods/utils.ts
+++ packages/app/non-translatable-markup/src/methods/utils.ts
@@ -12,13 +12,11 @@ export const nonTranslatableTagsRegexpG = new RegExp(nonTranslatableTagsPattern,
  *  `[...]` -> matches any one of the characters or Unicode property escapes specified within it
  *      `\p{P}` -> matches any kind of punctuation character
  *      `\p{S}` -> matches any kind of symbol character
- *      `\p{So}` -> matches any other symbol character
  *      `\p{Emoji}` -> matches any emoji character
- *      `\p{Math}` -> matches any math symbol character
  *  `u` -> enables unicode mode
  *
  * For more info see -> https://github.com/mdn/content/blob/main/files/en-us/web/javascript/reference/regular_expressions/unicode_character_class_escape/index.md
  */
-const symbolsPattern = /[\p{P}\p{S}\p{So}\p{Emoji}\p{Math}']/u
+const symbolsPattern = /[\p{P}\p{S}\p{Emoji}']/u
 
 export const symbolsRegexp = new RegExp(symbolsPattern)
