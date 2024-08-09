baec41b4e5fe73cb5d554b99674f31e342ba4721
CarlosGamero <carlos.gamero@lokalise.com>
2024-08-09 10:17:50 +0200

    vite.config is back

---
+++ packages/app/id-utils/vite.config.ts
@@ -0,0 +1,12 @@
+import { resolve } from 'node:path'
+
+import defineConfig from '@lokalise/package-vite-config/package'
+
+// @ts-ignore
+import packageJson from './package.json'
+
+// biome-ignore lint/style/noDefaultExport: <explanation>
+export default defineConfig({
+  entry: resolve(__dirname, 'src/index.ts'),
+  dependencies: Object.keys(packageJson.dependencies),
+})
