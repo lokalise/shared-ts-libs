3db452d0178461bee65859dc4f786a814e4f17b4
CarlosGamero <carlos.gamero@lokalise.com>
2024-09-04 12:53:01 +0200

    script utils migrated

--- packages/app/script-utils/vite.config.ts
+++ packages/app/script-utils/vite.config.ts
@@ -2,10 +2,11 @@ import { resolve } from 'node:path'
 
 import defineConfig from '@lokalise/package-vite-config/package'
 
+// @ts-ignore
 import packageJson from './package.json'
 
-/* eslint-disable import/no-default-export */
+// biome-ignore lint/style/noDefaultExport: <explanation>
 export default defineConfig({
-	entry: resolve(__dirname, 'src/index.ts'),
-	dependencies: Object.keys(packageJson.dependencies),
+  entry: resolve(__dirname, 'src/index.ts'),
+  dependencies: Object.keys(packageJson.dependencies),
 })
