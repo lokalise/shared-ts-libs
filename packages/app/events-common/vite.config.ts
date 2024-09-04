c932a7b4d048ab4299872ff6898c9a5e222dc00d
CarlosGamero <carlos.gamero@lokalise.com>
2024-09-04 13:06:35 +0200

    events-common biome

--- packages/app/events-common/vite.config.ts
+++ packages/app/events-common/vite.config.ts
@@ -2,14 +2,16 @@ import { resolve } from 'node:path'
 
 import defineConfig from '@lokalise/package-vite-config/package'
 
+// @ts-ignore
 import packageJson from './package.json'
 
-/* eslint-disable import/no-default-export */
+// biome-ignore lint/style/noDefaultExport: <explanation>
 export default defineConfig({
   entry: resolve(__dirname, 'src/index.ts'),
   dependencies: Object.keys(packageJson.peerDependencies),
   test: {
     coverage: {
+      provider: 'v8',
       exclude: ['src/**/index.ts', 'src/**/*.spec.ts', 'src/**/*.test.ts', 'src/**/*types.ts'],
       thresholds: {
         lines: 96,
