db19fb850f9dcd797289e38e4911f47557dad09a
CarlosGamero <carlos.gamero@lokalise.com>
2024-09-19 16:31:37 +0200

    Adding areEquals

---
+++ packages/app/universal-ts-utils/src/object/areEquals.ts
@@ -0,0 +1,30 @@
+/**
+ * TODO: if we decide to go with this, add tests + doc
+ */
+export const areEquals = (a: unknown, b: unknown): boolean => {
+  if (a === b) return true
+
+  // both should be defined at this point
+  // if both are nullable but with different values (null, undefined) they are not equal
+  if (!a || !b) return false
+
+  // if both are arrays, compare them recursively
+  if (Array.isArray(a) && Array.isArray(b)) return areArraysEqual(a, b)
+
+  // if both are objects, compare them recursively
+  if (typeof a === 'object' && typeof b === 'object') {
+    return Object.keys(a).every((k) => {
+      if (!Object.hasOwn(b, k)) return false
+      // @ts-ignore
+      return areEquals(a[k], b[k])
+    })
+  }
+
+  return false
+}
+
+const areArraysEqual = (a: unknown[], b: unknown[]): boolean => {
+  if (a.length !== b.length) return false
+
+  return a.every((value, index) => areEquals(value, b[index]))
+}
