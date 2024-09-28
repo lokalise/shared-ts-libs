78070abb56abc07d46644ee312463f8542bf36b3
CarlosGamero <carlos.gamero@lokalise.com>
2024-09-28 18:22:03 +0200

    sortByField method

---
+++ packages/app/universal-ts-utils/src/array/sortByField.spec.ts
@@ -0,0 +1,9 @@
+import { describe, it } from 'vitest'
+import { sortByField } from './sortByField'
+
+// TODO
+describe('sortByField', () => {
+  it('test', () => {
+    sortByField([{ id: '' }], 'id')
+  })
+})
---
+++ packages/app/universal-ts-utils/src/array/sortByField.ts
@@ -0,0 +1,18 @@
+type KeysMatching<T, V> = {
+  [K in keyof T]: T[K] extends V ? K : never
+}[keyof T]
+
+// TODO: add doc
+export const sortByField = <
+  T extends object,
+  K extends KeysMatching<T, string | number | symbol | null | undefined>,
+>(
+  array: T[],
+  field: K,
+  order: 'asc' | 'desc' = 'asc',
+): T[] =>
+  order === 'asc'
+    ? // @ts-expect-error
+      array.sort((a, b) => a[field].localeCompare(b[field]))
+    : // @ts-expect-error
+      array.sort((a, b) => b[field].localeCompare(a[field]))
