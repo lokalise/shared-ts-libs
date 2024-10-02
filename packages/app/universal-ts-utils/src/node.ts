4e98a804958c19374bb97a8fe2550c5018880cb3
CarlosGamero <carlos.gamero@lokalise.com>
2024-10-02 19:53:40 +0200

    Adding exports

--- packages/app/universal-ts-utils/src/node.ts
+++ packages/app/universal-ts-utils/src/node.ts
@@ -3,10 +3,13 @@
 // array
 export * from './array/areStringArraysEqual.js'
 export * from './array/chunk.js'
+export * from './array/callChunked.js'
 export * from './array/isNonEmptyArray.js'
 export * from './array/removeFalsy.js'
 export * from './array/removeNullish.js'
 export * from './array/unique.js'
+export * from './array/sort.js'
+export * from './array/sortByField.js'
 
 // object
 export * from './object/areEquals.js'
