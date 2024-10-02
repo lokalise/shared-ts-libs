aadc26130afce14f7639be8ecd9349c0ef28694f
CarlosGamero <carlos.gamero@lokalise.com>
2024-10-02 19:45:32 +0200

    removeDuplicates method

---
+++ packages/app/universal-ts-utils/src/array/removeDuplicates.ts
@@ -0,0 +1,12 @@
+type NonObject = string | number | boolean | null | undefined | symbol | bigint
+
+/**
+ * Return a copy of the given array without duplicates.
+ *
+ * @template T - The type of elements within the array.
+ * @param {T[]} array - The array to be deduplicated.
+ * @returns {T[]} - Returns param array without duplicates.
+ */
+export const removeDuplicates = <T extends NonObject>(array: readonly T[]): T[] => [
+  ...new Set(array),
+]
