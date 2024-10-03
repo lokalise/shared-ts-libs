01555cb44d3a219e40cf45ebd1c86487e836101f
CarlosGamero <carlos.gamero@lokalise.com>
2024-10-03 20:20:25 +0200

    Sort method and tests

--- packages/app/universal-ts-utils/src/array/sort.ts
+++ packages/app/universal-ts-utils/src/array/sort.ts
@@ -1,9 +1,39 @@
-type NonObject = string | number | boolean | null | undefined | symbol | bigint
+/**
+ * Sorts an array of primitive types in either ascending or descending order.
+ *
+ * @template T The type of the elements in the array.
+ *
+ * @param {T[]} array - The array to be sorted. This should be an array of primitive values.
+ * @param {'asc' | 'desc'} [order='asc'] - The order in which to sort the array. Defaults to 'asc' if not specified.
+ *
+ * @returns {T[]} The sorted array in the specified order.
+ */
+export const sort = <T extends string | number | boolean>(
+  array: T[],
+  order: 'asc' | 'desc' = 'asc',
+): T[] =>
+  order === 'asc' ? array.sort((a, b) => compare(a, b)) : array.sort((a, b) => compare(b, a))
 
-// TODO: add doc
-export const sort = <T extends NonObject>(array: T[], order: 'asc' | 'desc' = 'asc'): T[] =>
-  order === 'asc'
-    ? // @ts-expect-error
-      array.sort((a, b) => a.localeCompare(b))
-    : // @ts-expect-error
-      array.sort((a, b) => b.localeCompare(a))
+export const compare = <T extends string | number | boolean>(a: T, b: T): number => {
+  if (a === b) return 0
+
+  // same types comparison
+  if (typeof a === 'string' && typeof b === 'string') return a.localeCompare(b)
+  if (typeof a === 'number' && typeof b === 'number') return a - b
+  if (typeof a === 'boolean' && typeof b === 'boolean') return Number(a) - Number(b)
+
+  // number like comparison
+  const [aNumeric, bNumeric] = [a, b].map((e) => Number(e))
+  if (isNumericValue(aNumeric) && isNumericValue(bNumeric)) {
+    // converted booleans has less priority than numbers or numeric strings
+    if (typeof b === 'boolean') return 1
+    if (typeof a === 'boolean') return -1
+
+    return aNumeric - bNumeric
+  }
+
+  return String(a).localeCompare(String(b))
+}
+
+const isNumericValue = (value: unknown): value is number =>
+  typeof value === 'number' && !Number.isNaN(value)
