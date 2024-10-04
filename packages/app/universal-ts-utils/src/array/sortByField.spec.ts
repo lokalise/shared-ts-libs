3dbff43c772d723f7b955dd8ebbf023f8bc658b2
CarlosGamero <carlos.gamero@lokalise.com>
2024-10-04 20:35:55 +0200

    Finishig sortByField first version

--- packages/app/universal-ts-utils/src/array/sortByField.spec.ts
+++ packages/app/universal-ts-utils/src/array/sortByField.spec.ts
@@ -1,9 +1,73 @@
-import { describe, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { sortByField } from './sortByField'
 
-// TODO
 describe('sortByField', () => {
-  it('test', () => {
-    sortByField([{ id: '' }], 'id')
+  it('should handle an empty array without throwing errors', () => {
+    const data: { name: string }[] = []
+    const sortedData = sortByField(data, 'name')
+
+    expect(sortedData).toEqual([])
+  })
+
+  it('should sort objects based string field', () => {
+    const data = [
+      { name: 'Zara', age: 22 },
+      { name: 'Alex', age: 30 },
+      { name: 'John', age: 25 },
+    ]
+
+    expect(sortByField(data, 'name')).toEqual([
+      { name: 'Alex', age: 30 },
+      { name: 'John', age: 25 },
+      { name: 'Zara', age: 22 },
+    ])
+    expect(sortByField(data, 'name', 'desc')).toEqual([
+      { name: 'Zara', age: 22 },
+      { name: 'John', age: 25 },
+      { name: 'Alex', age: 30 },
+    ])
+  })
+
+  it('should throw if field is not a string', () => {
+    const data = [
+      { name: 'Charlie', age: 22 },
+      { name: 'Bob', age: 25 },
+    ]
+
+    // @ts-expect-error Testing for incorrect field type
+    expect(() => sortByField(data, 'age')).toThrow()
+  })
+
+  it('should throw an error if string field does not exist', () => {
+    const data = [
+      { name: 'Charlie', age: 22 },
+      { name: 'Bob', age: 25 },
+    ]
+
+    expect(() => sortByField(data, 'nonExistentField' as any)).toThrow()
+  })
+
+  it('should perform a correct case-insensitive sort', () => {
+    const data = [{ name: 'apple' }, { name: 'Banana' }, { name: 'cherry' }]
+
+    const sortedData = sortByField(data, 'name')
+
+    expect(sortedData).toEqual([{ name: 'apple' }, { name: 'Banana' }, { name: 'cherry' }])
+  })
+
+  it('should correctly handle sorting fields with the same value', () => {
+    const data = [{ name: 'alpha' }, { name: 'alpha' }, { name: 'beta' }]
+
+    const sortedData = sortByField(data, 'name')
+
+    expect(sortedData).toEqual([{ name: 'alpha' }, { name: 'alpha' }, { name: 'beta' }])
+  })
+
+  it('should handle arrays with a single element', () => {
+    const data = [{ name: 'single', age: 40 }]
+
+    const sortedData = sortByField(data, 'name')
+
+    expect(sortedData).toEqual([{ name: 'single', age: 40 }])
   })
 })
--- packages/app/universal-ts-utils/src/array/sortByField.ts
+++ packages/app/universal-ts-utils/src/array/sortByField.ts
@@ -1,9 +1,43 @@
-type KeysMatching<T, V> = {
+type KeysMatching<T extends object, V> = {
   [K in keyof T]: T[K] extends V ? K : never
 }[keyof T]
 
-// TODO: add doc
-export const sortByField = <T extends object, K extends KeysMatching<T, string | number | boolean>>(
+/*
+ TODO: Extend this method more types of fields.
+ For now, it only supports fields of type string as it is the most common use case.
+ the objective is to use the compare method defined on `sort.ts` but don't want to expose it at this point.
+
+ So in a follow-up will try to come up with a solution to have "internal" not exposed utilities, and then extends this
+ method.
+*/
+
+/**
+ * Sorts an array of objects based on a specified string field and order.
+ *
+ * @template T - The type of the objects within the array.
+ * @template K - The key of T that maps to a string value.
+ *
+ * @param {T[]} array - The array of objects to be sorted.
+ * @param {K} field - The field of the objects which is of type string, used to define the sort criterion.
+ * @param {'asc' | 'desc'} [order='asc'] - The order in which to sort the array. Defaults to 'asc'.
+ *
+ * @returns {T[]} The sorted array of objects.
+ *
+ * @example
+ * ```typescript
+ * const data = [
+ *   { name: 'Zara', age: 22 },
+ *   { name: 'Alex', age: 30 },
+ *   { name: 'John', age: 25 }
+ * ];
+ *
+ * // Sort by 'name'
+ * const sortedByName = sortByField(data, 'name')
+ * // Output: [ { name: 'Alex', age: 30 }, { name: 'John', age: 25 }, { name: 'Zara', age: 22 } ]
+ * console.log(sortedByName)
+ * ```
+ */
+export const sortByField = <T extends object, K extends KeysMatching<T, string>>(
   array: T[],
   field: K,
   order: 'asc' | 'desc' = 'asc',
