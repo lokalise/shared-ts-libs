d7933b468f15a80acccca1e0085e56036b462aec
CarlosGamero <carlos.gamero@lokalise.com>
2024-11-06 13:45:34 +0100

    Name change

--- packages/app/universal-ts-utils/src/node.ts
+++ packages/app/universal-ts-utils/src/node.ts
@@ -1,15 +1,15 @@
 // We don't want to have a typical index file due to how FE bundlers work. Please see `readme` for more info.
 
 // array
-export * from './external/array/areStringArraysEqual.js'
-export * from './external/array/chunk.js'
-export * from './external/array/callChunked.js'
-export * from './external/array/isNonEmptyArray.js'
-export * from './external/array/removeFalsy.js'
-export * from './external/array/removeNullish.js'
-export * from './external/array/unique.js'
-export * from './external/array/sort.js'
-export * from './external/array/sortByField.js'
+export * from './public/array/areStringArraysEqual.js'
+export * from './public/array/chunk.js'
+export * from './public/array/callChunked.js'
+export * from './public/array/isNonEmptyArray.js'
+export * from './public/array/removeFalsy.js'
+export * from './public/array/removeNullish.js'
+export * from './public/array/unique.js'
+export * from './public/array/sort.js'
+export * from './public/array/sortByField.js'
 
 // object
-export * from './external/object/areDeepEqual.js'
+export * from './public/object/areDeepEqual.js'
