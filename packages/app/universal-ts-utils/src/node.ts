f2b7b41834794478636e2f6e05bfb673475c0b43
CarlosGamero <carlos.gamero@lokalise.com>
2024-11-06 13:30:19 +0100

    Creating external folder

--- packages/app/universal-ts-utils/src/node.ts
+++ packages/app/universal-ts-utils/src/node.ts
@@ -1,15 +1,15 @@
 // We don't want to have a typical index file due to how FE bundlers work. Please see `readme` for more info.
 
 // array
-export * from './array/areStringArraysEqual.js'
-export * from './array/chunk.js'
-export * from './array/callChunked.js'
-export * from './array/isNonEmptyArray.js'
-export * from './array/removeFalsy.js'
-export * from './array/removeNullish.js'
-export * from './array/unique.js'
-export * from './array/sort.js'
-export * from './array/sortByField.js'
+export * from './external/array/areStringArraysEqual.js'
+export * from './external/array/chunk.js'
+export * from './external/array/callChunked.js'
+export * from './external/array/isNonEmptyArray.js'
+export * from './external/array/removeFalsy.js'
+export * from './external/array/removeNullish.js'
+export * from './external/array/unique.js'
+export * from './external/array/sort.js'
+export * from './external/array/sortByField.js'
 
 // object
-export * from './object/areDeepEqual.js'
+export * from './external/object/areDeepEqual.js'
