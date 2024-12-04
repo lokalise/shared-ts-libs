74c251f4df4d13f380746c4e53c5c0d9dd898414
CarlosGamero <carlos.gamero@lokalise.com>
2024-12-04 16:54:09 +0100

    Adding deepClone

--- packages/app/universal-ts-utils/src/node.ts
+++ packages/app/universal-ts-utils/src/node.ts
@@ -14,6 +14,7 @@ export * from './public/array/sortByField.js'
 // object
 export * from './public/object/areDeepEqual.js'
 export * from './public/object/convertDateFieldsToIsoString.js'
+export * from './public/object/deepClone.js'
 export * from './public/object/groupBy.js'
 export * from './public/object/groupByUnique.js'
 export * from './public/object/transformToKebabCase.js'
