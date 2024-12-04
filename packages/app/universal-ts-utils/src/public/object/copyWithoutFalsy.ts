7aafae931bc00c2cce71140898d604aebc9674c1
CarlosGamero <carlos.gamero@lokalise.com>
2024-12-04 18:16:44 +0100

    Improving doc

--- packages/app/universal-ts-utils/src/public/object/copyWithoutFalsy.ts
+++ packages/app/universal-ts-utils/src/public/object/copyWithoutFalsy.ts
@@ -7,6 +7,17 @@ type Output<T extends Record<RecordKeyType, unknown>> = Pick<
   }[keyof T]
 >
 
+/**
+ * Creates a shallow copy of an object, excluding properties with "falsy" values.
+ *
+ * A "falsy" value includes `null`, `undefined`, empty strings (`''`), and other JavaScript
+ * falsy values like `false`, `0`, `NaN`, and `""`. This function specifically removes the
+ * explicit `null`, `undefined`, and empty string values.
+ *
+ * @param {Record} object - The source object from which to copy properties.
+ * @returns {Record} A new object containing only the properties from the source object
+ *    that do not have "falsy" values.
+ */
 export const copyWithoutFalsy = <T extends Record<RecordKeyType, unknown>>(object: T): Output<T> =>
   Object.keys(object).reduce(
     (acc, key) => {
