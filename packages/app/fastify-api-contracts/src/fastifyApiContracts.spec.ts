50a948edc962532305dc300223d1fed47d8df563
CarlosGamero <carlos.gamero@lokalise.com>
2025-02-14 17:57:25 +0100

    Dist import fix

--- packages/app/fastify-api-contracts/src/fastifyApiContracts.spec.ts
+++ packages/app/fastify-api-contracts/src/fastifyApiContracts.spec.ts
@@ -1,27 +1,27 @@
-import {buildGetController} from "./fastifyApiContracts";
-import {buildGetRoute} from "@lokalise/universal-ts-utils/dist/public/api-contracts/apiContracts";
-import {z} from "zod";
-import {expect} from "vitest";
+import { buildGetRoute } from '@lokalise/universal-ts-utils/node'
+import { z } from 'zod'
+import { buildGetController } from './fastifyApiContracts'
 
 const BODY_SCHEMA = z.object({})
 const PATH_PARAMS_SCHEMA = z.object({
-    userId: z.string(),
+  userId: z.string(),
 })
-const PATH_PARAMS_MULTI_SCHEMA = z.object({
-    userId: z.string(),
-    orgId: z.string(),
+const _PATH_PARAMS_MULTI_SCHEMA = z.object({
+  userId: z.string(),
+  orgId: z.string(),
 })
 
 describe('fastifyApiContracts', () => {
-    describe('buildGetRoute', () => {
-        const contract = buildGetRoute({
-            responseBodySchema: BODY_SCHEMA,
-            requestPathParamsSchema: PATH_PARAMS_SCHEMA,
-            pathResolver: (pathParams) => `/users/${pathParams.userId}`,
-        })
-
-        const controller = buildGetController(contract, (req) => {
-            expect(req.params.userId).toEqual('1')
-        })
+  describe('buildGetRoute', () => {
+    const contract = buildGetRoute({
+      responseBodySchema: BODY_SCHEMA,
+      requestPathParamsSchema: PATH_PARAMS_SCHEMA,
+      pathResolver: (pathParams) => `/users/${pathParams.userId}`,
     })
+
+    const _controller = buildGetController(contract, (req) => {
+      expect(req.params.userId).toEqual('1')
+      return Promise.resolve()
+    })
+  })
 })
--- packages/app/fastify-api-contracts/src/fastifyApiContracts.ts
+++ packages/app/fastify-api-contracts/src/fastifyApiContracts.ts
@@ -1,86 +1,97 @@
-import type {FastifyReply, FastifyRequest, RouteOptions} from "fastify";
-import {GetRouteDefinition, mapRouteToPath} from "@lokalise/universal-ts-utils/dist/public/api-contracts/apiContracts";
-import {z, ZodSchema} from "zod";
-import type http from "node:http";
+import type http from 'node:http'
+import { type GetRouteDefinition, mapRouteToPath } from '@lokalise/universal-ts-utils/node'
+import type { FastifyReply, FastifyRequest, RouteOptions } from 'fastify'
+import type { ZodSchema } from 'zod'
 
 export type RouteType = RouteOptions<
-    http.Server,
-    http.IncomingMessage,
-    http.ServerResponse,
-    // biome-ignore lint/suspicious/noExplicitAny: it's ok
-    any,
-    // biome-ignore lint/suspicious/noExplicitAny: it's ok
-    any,
-    // biome-ignore lint/suspicious/noExplicitAny: it's ok
-    any,
-    // biome-ignore lint/suspicious/noExplicitAny: it's ok
-    any,
-    // biome-ignore lint/suspicious/noExplicitAny: it's ok
-    any
+  http.Server,
+  http.IncomingMessage,
+  http.ServerResponse,
+  // biome-ignore lint/suspicious/noExplicitAny: it's ok
+  any,
+  // biome-ignore lint/suspicious/noExplicitAny: it's ok
+  any,
+  // biome-ignore lint/suspicious/noExplicitAny: it's ok
+  any,
+  // biome-ignore lint/suspicious/noExplicitAny: it's ok
+  any,
+  // biome-ignore lint/suspicious/noExplicitAny: it's ok
+  any
 >
 
 export type FastifyControllerFn<ReplyType, BodyType, ParamsType, QueryType, HeadersType> = (
-    req: FastifyRequest<{
-        Body: BodyType,
-        Headers: HeadersType,
-        Params: ParamsType,
-        Querystring: QueryType,
-        Reply: ReplyType
-    }>,
-    reply: FastifyReply,
+  req: FastifyRequest<{
+    Body: BodyType
+    Headers: HeadersType
+    Params: ParamsType
+    Querystring: QueryType
+    Reply: ReplyType
+  }>,
+  reply: FastifyReply,
 ) => Promise<void>
 
 export type FastifyGetControllerFn<ReplyType, ParamsType, QueryType, HeadersType> = (
-    req: FastifyRequest<{
-        Body: never,
-        Headers: HeadersType,
-        Params: ParamsType,
-        Querystring: QueryType,
-        Reply: ReplyType
-    }>,
-    reply: FastifyReply < {Body: ReplyType} >,
+  req: FastifyRequest<{
+    Body: never
+    Headers: HeadersType
+    Params: ParamsType
+    Querystring: QueryType
+    Reply: ReplyType
+  }>,
+  reply: FastifyReply<{ Body: ReplyType }>,
 ) => Promise<void>
 
 export function buildGetController<
+  ResponseBodySchema,
+  PathParams,
+  RequestQuerySchema,
+  RequestHeaderSchema,
+>(
+  _apiContract: GetRouteDefinition<
+    PathParams,
+    ZodSchema<ResponseBodySchema>,
+    ZodSchema<PathParams>,
+    ZodSchema<RequestQuerySchema>,
+    ZodSchema<RequestHeaderSchema>
+  >,
+  handler: FastifyGetControllerFn<
     ResponseBodySchema,
     PathParams,
     RequestQuerySchema,
-    RequestHeaderSchema,
->(
-    _apiContract: GetRouteDefinition<
-        PathParams,
-        ZodSchema<ResponseBodySchema>,
-        ZodSchema<PathParams>,
-        ZodSchema<RequestQuerySchema>,
-        ZodSchema<RequestHeaderSchema>
-    >,
-    handler: FastifyGetControllerFn<ResponseBodySchema, PathParams, RequestQuerySchema, RequestHeaderSchema>
-): FastifyGetControllerFn<ResponseBodySchema, PathParams, RequestQuerySchema, RequestHeaderSchema>{
-    return handler
+    RequestHeaderSchema
+  >,
+): FastifyGetControllerFn<ResponseBodySchema, PathParams, RequestQuerySchema, RequestHeaderSchema> {
+  return handler
 }
 
 export function buildGetRoute<
+  ResponseBodySchema,
+  PathParams,
+  RequestQuerySchema,
+  RequestHeaderSchema,
+>(
+  apiContract: GetRouteDefinition<
+    PathParams,
+    ZodSchema<ResponseBodySchema>,
+    ZodSchema<PathParams>,
+    ZodSchema<RequestQuerySchema>,
+    ZodSchema<RequestHeaderSchema>
+  >,
+  handler: FastifyGetControllerFn<
     ResponseBodySchema,
     PathParams,
     RequestQuerySchema,
-    RequestHeaderSchema,
->(
-    apiContract: GetRouteDefinition<
-        PathParams,
-        ZodSchema<ResponseBodySchema>,
-        ZodSchema<PathParams>,
-        ZodSchema<RequestQuerySchema>,
-        ZodSchema<RequestHeaderSchema>
-    >,
-    handler: FastifyGetControllerFn<ResponseBodySchema, PathParams, RequestQuerySchema, RequestHeaderSchema>
-): RouteType  {
-    return {
-        method: apiContract.method,
-        url: mapRouteToPath(apiContract),
-        handler,
-        schema: { params: apiContract.requestPathParamsSchema,
-            // ToDo add description to apiContract
-            describe: 'Placeholder'
-        }
-    }
+    RequestHeaderSchema
+  >,
+): RouteType {
+  return {
+    method: apiContract.method,
+    url: mapRouteToPath(apiContract),
+    handler,
+    schema: {
+      params: apiContract.requestPathParamsSchema,
+      // ToDo add description to apiContract
+      describe: 'Placeholder',
+    },
+  }
 }
