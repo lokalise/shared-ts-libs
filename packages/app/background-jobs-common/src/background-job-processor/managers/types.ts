8a86cb0f9ab84abf3d7ec5cbd11d77aecf9c78db
CarlosGamero <carlos.gamero@lokalise.com>
2025-02-18 16:18:17 +0100

    EXP-128 id builder type

--- packages/app/background-jobs-common/src/background-job-processor/managers/types.ts
+++ packages/app/background-jobs-common/src/background-job-processor/managers/types.ts
@@ -9,14 +9,24 @@ export type QueueManagerConfig = {
   redisConfig: RedisConfig
 }
 
+type JobOptionsWithDeduplicationIdBuilder<JobOptionsType extends JobsOptions> = Omit<
+  JobOptionsType,
+  'deduplication'
+> & {
+  deduplication: Omit<JobOptionsType['deduplication'], 'id'> & {
+    // biome-ignore lint/suspicious/noExplicitAny: We cannot infer type of JobData and we have run time validation so any is the most flexible option
+    idBuilder: (JobData: any) => string
+  }
+}
+
 export type QueueConfiguration<
   QueueOptionsType extends QueueOptions = QueueOptions,
   JobOptionsType extends JobsOptions = JobsOptions,
 > = {
   queueId: string
-  queueOptions?: Omit<Partial<QueueOptionsType>, 'connection' | 'prefix'>
-  jobPayloadSchema: z.ZodType<BaseJobPayload> // should extend BASE_JOB_PAYLOAD_SCHEMA
-  jobOptions?: JobOptionsType
+  queueOptions?: Omit<QueueOptionsType, 'connection' | 'prefix'>
+  jobPayloadSchema: z.ZodType<BaseJobPayload> // should extend JobPayload
+  jobOptions?: JobOptionsWithDeduplicationIdBuilder<JobOptionsType>
 }
 
 export type SupportedQueueIds<Config extends QueueConfiguration[]> = Config[number]['queueId']
