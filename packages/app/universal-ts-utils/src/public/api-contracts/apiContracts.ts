7fdf552bfa021f0aefb922cf487e7f9d984f248a
Igor Savin <iselwin@gmail.com>
2025-02-19 14:48:04 +0200

    extract status codes

--- packages/app/universal-ts-utils/src/public/api-contracts/apiContracts.ts
+++ packages/app/universal-ts-utils/src/public/api-contracts/apiContracts.ts
@@ -1,71 +1,7 @@
 import type { ZodSchema, z } from 'zod'
+import type { HttpStatusCode } from './HttpStatusCodes.js'
 
 const EMPTY_PARAMS = {}
-
-export type HttpStatusCode =
-  | 100
-  | 101
-  | 102
-  | 103
-  | 200
-  | 201
-  | 202
-  | 203
-  | 204
-  | 205
-  | 206
-  | 207
-  | 208
-  | 226
-  | 300
-  | 301
-  | 302
-  | 303
-  | 304
-  | 305
-  | 306
-  | 307
-  | 308
-  | 400
-  | 401
-  | 402
-  | 403
-  | 404
-  | 405
-  | 406
-  | 407
-  | 408
-  | 409
-  | 410
-  | 411
-  | 412
-  | 413
-  | 414
-  | 415
-  | 416
-  | 417
-  | 418
-  | 421
-  | 422
-  | 423
-  | 424
-  | 425
-  | 426
-  | 428
-  | 429
-  | 431
-  | 451
-  | 500
-  | 501
-  | 502
-  | 503
-  | 504
-  | 505
-  | 506
-  | 507
-  | 508
-  | 510
-  | 511
 
 export type RoutePathResolver<PathParams> = (pathParams: PathParams) => string
 
