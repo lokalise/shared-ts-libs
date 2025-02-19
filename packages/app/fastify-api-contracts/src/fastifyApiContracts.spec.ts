07455f4d8ec3a6f61bbeff6d2e72d0dfdbc14fbd
Igor Savin <iselwin@gmail.com>
2025-02-19 11:02:59 +0200

    feat: support remaining HTTP methods for fastify api contracts (#478)

--- packages/app/fastify-api-contracts/src/fastifyApiContracts.spec.ts
+++ packages/app/fastify-api-contracts/src/fastifyApiContracts.spec.ts
@@ -1,15 +1,33 @@
-import { buildGetRoute } from '@lokalise/universal-ts-utils/node'
+import {
+  buildDeleteRoute,
+  buildGetRoute,
+  buildPayloadRoute,
+} from '@lokalise/universal-ts-utils/node'
 import { type RouteOptions, fastify } from 'fastify'
 import {
   type ZodTypeProvider,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
-import { describe, expect } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { z } from 'zod'
-import { buildFastifyGetRoute, buildFastifyGetRouteHandler } from './fastifyApiContracts'
-import { injectGet } from './fastifyApiRequestInjector'
+import {
+  buildFastifyNoPayloadRoute,
+  buildFastifyNoPayloadRouteHandler,
+  buildFastifyPayloadRoute,
+  buildFastifyPayloadRouteHandler,
+} from './fastifyApiContracts'
+import {
+  injectDelete,
+  injectGet,
+  injectPatch,
+  injectPost,
+  injectPut,
+} from './fastifyApiRequestInjector'
 
+const REQUEST_BODY_SCHEMA = z.object({
+  id: z.string(),
+})
 const BODY_SCHEMA = z.object({})
 const PATH_PARAMS_SCHEMA = z.object({
   userId: z.string(),
@@ -34,22 +52,19 @@ async function initApp(route: RouteOptions) {
 }
 
 describe('fastifyApiContracts', () => {
-  describe('buildFastifyGetRouteHandler', () => {
-    it('builds a handler', () => {
+  describe('buildFastifyNoPayloadRouteHandler', () => {
+    it('builds a GET handler', () => {
       const contract = buildGetRoute({
         responseBodySchema: BODY_SCHEMA,
         requestPathParamsSchema: PATH_PARAMS_SCHEMA,
         pathResolver: (pathParams) => `/users/${pathParams.userId}`,
       })
 
-      const handler = buildFastifyGetRouteHandler(contract, (req) => {
-        expect(req.params.userId).toEqual('1')
-        return Promise.resolve()
-      })
+      const handler = buildFastifyNoPayloadRouteHandler(contract, () => Promise.resolve())
       expect(handler).toBeTypeOf('function')
     })
   })
-  describe('buildFastifyGetRoute', () => {
+  describe('buildFastifyNoPayloadRoute', () => {
     it('uses API spec to build valid GET route in fastify app', async () => {
       expect.assertions(2)
       const contract = buildGetRoute({
@@ -58,7 +73,7 @@ describe('fastifyApiContracts', () => {
         pathResolver: (pathParams) => `/users/${pathParams.userId}`,
       })
 
-      const route = buildFastifyGetRoute(contract, (req) => {
+      const route = buildFastifyNoPayloadRoute(contract, (req) => {
         expect(req.params.userId).toEqual('1')
         return Promise.resolve()
       })
@@ -66,6 +81,118 @@ describe('fastifyApiContracts', () => {
       const app = await initApp(route)
       const response = await injectGet(app, contract, {
         pathParams: { userId: '1' },
+      })
+
+      expect(response.statusCode).toBe(200)
+    })
+
+    it('uses API spec to build valid DELETE route in fastify app', async () => {
+      expect.assertions(2)
+      const contract = buildDeleteRoute({
+        responseBodySchema: BODY_SCHEMA,
+        requestPathParamsSchema: PATH_PARAMS_SCHEMA,
+        pathResolver: (pathParams) => `/users/${pathParams.userId}`,
+      })
+
+      const route = buildFastifyNoPayloadRoute(contract, (req) => {
+        expect(req.params.userId).toEqual('1')
+        return Promise.resolve()
+      })
+
+      const app = await initApp(route)
+      const response = await injectDelete(app, contract, {
+        pathParams: { userId: '1' },
+      })
+
+      expect(response.statusCode).toBe(200)
+    })
+  })
+
+  describe('buildFastifyPayloadRouteHandler', () => {
+    it('builds a POST handler', () => {
+      const contract = buildPayloadRoute({
+        method: 'post',
+        requestBodySchema: REQUEST_BODY_SCHEMA,
+        responseBodySchema: BODY_SCHEMA,
+        requestPathParamsSchema: PATH_PARAMS_SCHEMA,
+        pathResolver: (pathParams) => `/users/${pathParams.userId}`,
+      })
+
+      const handler = buildFastifyPayloadRouteHandler(contract, () => Promise.resolve())
+      expect(handler).toBeTypeOf('function')
+    })
+  })
+  describe('buildFastifyPayloadRoute', () => {
+    it('uses API spec to build valid POST route in fastify app', async () => {
+      expect.assertions(3)
+      const contract = buildPayloadRoute({
+        method: 'post',
+        requestBodySchema: REQUEST_BODY_SCHEMA,
+        responseBodySchema: BODY_SCHEMA,
+        requestPathParamsSchema: PATH_PARAMS_SCHEMA,
+        pathResolver: (pathParams) => `/users/${pathParams.userId}`,
+      })
+
+      const route = buildFastifyPayloadRoute(contract, (req) => {
+        expect(req.params.userId).toEqual('1')
+        expect(req.body.id).toEqual('2')
+        return Promise.resolve()
+      })
+
+      const app = await initApp(route)
+      const response = await injectPost(app, contract, {
+        pathParams: { userId: '1' },
+        body: { id: '2' },
+      })
+
+      expect(response.statusCode).toBe(200)
+    })
+
+    it('uses API spec to build valid PATCH route in fastify app', async () => {
+      expect.assertions(3)
+      const contract = buildPayloadRoute({
+        method: 'patch',
+        requestBodySchema: REQUEST_BODY_SCHEMA,
+        responseBodySchema: BODY_SCHEMA,
+        requestPathParamsSchema: PATH_PARAMS_SCHEMA,
+        pathResolver: (pathParams) => `/users/${pathParams.userId}`,
+      })
+
+      const route = buildFastifyPayloadRoute(contract, (req) => {
+        expect(req.params.userId).toEqual('1')
+        expect(req.body.id).toEqual('2')
+        return Promise.resolve()
+      })
+
+      const app = await initApp(route)
+      const response = await injectPatch(app, contract, {
+        pathParams: { userId: '1' },
+        body: { id: '2' },
+      })
+
+      expect(response.statusCode).toBe(200)
+    })
+
+    it('uses API spec to build valid PUT route in fastify app', async () => {
+      expect.assertions(3)
+      const contract = buildPayloadRoute({
+        method: 'put',
+        requestBodySchema: REQUEST_BODY_SCHEMA,
+        responseBodySchema: BODY_SCHEMA,
+        requestPathParamsSchema: PATH_PARAMS_SCHEMA,
+        pathResolver: (pathParams) => `/users/${pathParams.userId}`,
+      })
+
+      const route = buildFastifyPayloadRoute(contract, (req) => {
+        expect(req.params.userId).toEqual('1')
+        expect(req.body.id).toEqual('2')
+        return Promise.resolve()
+      })
+
+      const app = await initApp(route)
+      const response = await injectPut(app, contract, {
+        pathParams: { userId: '1' },
+        body: { id: '2' },
       })
 
       expect(response.statusCode).toBe(200)
--- packages/app/fastify-api-contracts/src/fastifyApiContracts.ts
+++ packages/app/fastify-api-contracts/src/fastifyApiContracts.ts
@@ -1,8 +1,19 @@
 import type http from 'node:http'
 import { copyWithoutUndefined } from '@lokalise/node-core'
-import { type GetRouteDefinition, mapRouteToPath } from '@lokalise/universal-ts-utils/node'
+import {
+  type DeleteRouteDefinition,
+  type GetRouteDefinition,
+  type PayloadRouteDefinition,
+  mapRouteToPath,
+} from '@lokalise/universal-ts-utils/node'
 import type { FastifyReply, FastifyRequest, RouteOptions } from 'fastify'
+import type { FastifySchema } from 'fastify/types/schema'
 import type { ZodSchema } from 'zod'
+
+/**
+ * Default fastify fields + fastify-swagger fields
+ */
+export type ExtendedFastifySchema = FastifySchema & { describe?: string }
 
 export type RouteType = RouteOptions<
   http.Server,
@@ -20,6 +31,9 @@ export type RouteType = RouteOptions<
   any
 >
 
+/**
+ * Handler for POST, PUT and PATCH methods
+ */
 export type FastifyPayloadHandlerFn<ReplyType, BodyType, ParamsType, QueryType, HeadersType> = (
   req: FastifyRequest<{
     Body: BodyType
@@ -31,7 +45,10 @@ export type FastifyPayloadHandlerFn<ReplyType, BodyType, ParamsType, QueryType, 
   reply: FastifyReply,
 ) => Promise<void>
 
-export type FastifyGetHandlerFn<ReplyType, ParamsType, QueryType, HeadersType> = (
+/**
+ * Handler for GET and DELETE methods
+ */
+export type FastifyNoPayloadHandlerFn<ReplyType, ParamsType, QueryType, HeadersType> = (
   req: FastifyRequest<{
     Body: never
     Headers: HeadersType
@@ -43,45 +60,73 @@ export type FastifyGetHandlerFn<ReplyType, ParamsType, QueryType, HeadersType> =
 ) => Promise<void>
 
 /**
- * Infers handler request type automatically from the contract
+ * Infers handler request type automatically from the contract for GET or DELETE methods
  */
-export function buildFastifyGetRouteHandler<
+export function buildFastifyNoPayloadRouteHandler<
   ResponseBodySchema,
   PathParams,
   RequestQuerySchema,
   RequestHeaderSchema,
 >(
-  _apiContract: GetRouteDefinition<
-    PathParams,
-    ZodSchema<ResponseBodySchema>,
-    ZodSchema<PathParams>,
-    ZodSchema<RequestQuerySchema>,
-    ZodSchema<RequestHeaderSchema>
-  >,
-  handler: FastifyGetHandlerFn<
+  _apiContract:
+    | GetRouteDefinition<
+        PathParams,
+        ZodSchema<ResponseBodySchema>,
+        ZodSchema<PathParams>,
+        ZodSchema<RequestQuerySchema>,
+        ZodSchema<RequestHeaderSchema>
+      >
+    | DeleteRouteDefinition<
+        PathParams,
+        ZodSchema<ResponseBodySchema>,
+        ZodSchema<PathParams>,
+        ZodSchema<RequestQuerySchema>,
+        ZodSchema<RequestHeaderSchema>
+      >,
+  handler: FastifyNoPayloadHandlerFn<
     ResponseBodySchema,
     PathParams,
     RequestQuerySchema,
     RequestHeaderSchema
   >,
-): FastifyGetHandlerFn<ResponseBodySchema, PathParams, RequestQuerySchema, RequestHeaderSchema> {
+): FastifyNoPayloadHandlerFn<
+  ResponseBodySchema,
+  PathParams,
+  RequestQuerySchema,
+  RequestHeaderSchema
+> {
   return handler
 }
 
-export function buildFastifyGetRoute<
+/**
+ * Build full fastify route definition for GET or DELETE methods
+ */
+export function buildFastifyNoPayloadRoute<
   ResponseBodySchema,
   PathParams,
   RequestQuerySchema,
   RequestHeaderSchema,
 >(
-  apiContract: GetRouteDefinition<
-    PathParams,
-    ZodSchema<ResponseBodySchema>,
-    ZodSchema<PathParams>,
-    ZodSchema<RequestQuerySchema>,
-    ZodSchema<RequestHeaderSchema>
-  >,
-  handler: FastifyGetHandlerFn<
+  apiContract:
+    | GetRouteDefinition<
+        PathParams,
+        ZodSchema<ResponseBodySchema>,
+        ZodSchema<PathParams>,
+        ZodSchema<RequestQuerySchema>,
+        ZodSchema<RequestHeaderSchema>,
+        boolean,
+        boolean
+      >
+    | DeleteRouteDefinition<
+        PathParams,
+        ZodSchema<ResponseBodySchema>,
+        ZodSchema<PathParams>,
+        ZodSchema<RequestQuerySchema>,
+        ZodSchema<RequestHeaderSchema>,
+        boolean,
+        boolean
+      >,
+  handler: FastifyNoPayloadHandlerFn<
     ResponseBodySchema,
     PathParams,
     RequestQuerySchema,
@@ -97,6 +142,81 @@ export function buildFastifyGetRoute<
       querystring: apiContract.requestQuerySchema,
       headers: apiContract.requestHeaderSchema,
       describe: apiContract.description,
-    }),
+    } satisfies ExtendedFastifySchema),
+  }
+}
+
+/**
+ * Infers handler request type automatically from the contract for POST, PUT and PATCH methods
+ */
+export function buildFastifyPayloadRouteHandler<
+  RequestBodySchema,
+  ResponseBodySchema,
+  PathParams,
+  RequestQuerySchema,
+  RequestHeaderSchema,
+>(
+  _apiContract: PayloadRouteDefinition<
+    PathParams,
+    ZodSchema<RequestBodySchema>,
+    ZodSchema<ResponseBodySchema>,
+    ZodSchema<PathParams>,
+    ZodSchema<RequestQuerySchema>,
+    ZodSchema<RequestHeaderSchema>
+  >,
+  handler: FastifyPayloadHandlerFn<
+    ResponseBodySchema,
+    RequestBodySchema,
+    PathParams,
+    RequestQuerySchema,
+    RequestHeaderSchema
+  >,
+): FastifyPayloadHandlerFn<
+  ResponseBodySchema,
+  RequestBodySchema,
+  PathParams,
+  RequestQuerySchema,
+  RequestHeaderSchema
+> {
+  return handler
+}
+
+/**
+ * Build full fastify route definition for POST, PUT and PATCH methods
+ */
+export function buildFastifyPayloadRoute<
+  RequestBodySchema,
+  ResponseBodySchema,
+  PathParams,
+  RequestQuerySchema,
+  RequestHeaderSchema,
+>(
+  apiContract: PayloadRouteDefinition<
+    PathParams,
+    ZodSchema<RequestBodySchema>,
+    ZodSchema<ResponseBodySchema>,
+    ZodSchema<PathParams>,
+    ZodSchema<RequestQuerySchema>,
+    ZodSchema<RequestHeaderSchema>
+  >,
+  handler: FastifyPayloadHandlerFn<
+    ResponseBodySchema,
+    RequestBodySchema,
+    PathParams,
+    RequestQuerySchema,
+    RequestHeaderSchema
+  >,
+): RouteType {
+  return {
+    method: apiContract.method,
+    url: mapRouteToPath(apiContract),
+    handler,
+    schema: copyWithoutUndefined({
+      body: apiContract.requestBodySchema,
+      params: apiContract.requestPathParamsSchema,
+      querystring: apiContract.requestQuerySchema,
+      headers: apiContract.requestHeaderSchema,
+      describe: apiContract.description,
+    } satisfies ExtendedFastifySchema),
   }
 }
