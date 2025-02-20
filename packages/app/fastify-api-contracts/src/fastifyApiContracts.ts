fb3c4f2a258c28f17213db13daf34c723c02b7c0
CarlosGamero <carlos.gamero@lokalise.com>
2025-02-20 20:00:44 +0100

    Moving types to a separate file

--- packages/app/fastify-api-contracts/src/fastifyApiContracts.ts
+++ packages/app/fastify-api-contracts/src/fastifyApiContracts.ts
@@ -6,58 +6,14 @@ import {
   type PayloadRouteDefinition,
   mapRouteToPath,
 } from '@lokalise/universal-ts-utils/node'
-import type { FastifyReply, FastifyRequest, RouteOptions } from 'fastify'
-import type { FastifySchema } from 'fastify/types/schema'
 import type { ZodSchema } from 'zod'
-
-/**
- * Default fastify fields + fastify-swagger fields
- */
-export type ExtendedFastifySchema = FastifySchema & { describe?: string }
-
-export type RouteType = RouteOptions<
-  http.Server,
-  http.IncomingMessage,
-  http.ServerResponse,
-  // biome-ignore lint/suspicious/noExplicitAny: it's ok
-  any,
-  // biome-ignore lint/suspicious/noExplicitAny: it's ok
-  any,
-  // biome-ignore lint/suspicious/noExplicitAny: it's ok
-  any,
-  // biome-ignore lint/suspicious/noExplicitAny: it's ok
-  any,
-  // biome-ignore lint/suspicious/noExplicitAny: it's ok
-  any
->
-
-/**
- * Handler for POST, PUT and PATCH methods
- */
-export type FastifyPayloadHandlerFn<ReplyType, BodyType, ParamsType, QueryType, HeadersType> = (
-  req: FastifyRequest<{
-    Body: BodyType
-    Headers: HeadersType
-    Params: ParamsType
-    Querystring: QueryType
-    Reply: ReplyType
-  }>,
-  reply: FastifyReply,
-) => Promise<void>
-
-/**
- * Handler for GET and DELETE methods
- */
-export type FastifyNoPayloadHandlerFn<ReplyType, ParamsType, QueryType, HeadersType> = (
-  req: FastifyRequest<{
-    Body: never
-    Headers: HeadersType
-    Params: ParamsType
-    Querystring: QueryType
-    Reply: ReplyType
-  }>,
-  reply: FastifyReply<{ Body: ReplyType }>,
-) => Promise<void>
+import type {
+  ApiContractMetadataToRouteMapper,
+  ExtendedFastifySchema,
+  FastifyNoPayloadHandlerFn,
+  FastifyPayloadHandlerFn,
+  RouteType,
+} from './types.js'
 
 /**
  * Infers handler request type automatically from the contract for GET or DELETE methods
---
+++ packages/app/fastify-api-contracts/src/types.ts
@@ -0,0 +1,52 @@
+import type http from 'node:http'
+import type { FastifyReply, FastifyRequest, RouteOptions } from 'fastify'
+import type { FastifySchema } from 'fastify/types/schema'
+
+/**
+ * Default fastify fields + fastify-swagger fields
+ */
+export type ExtendedFastifySchema = FastifySchema & { describe?: string }
+
+export type RouteType = RouteOptions<
+  http.Server,
+  http.IncomingMessage,
+  http.ServerResponse,
+  // biome-ignore lint/suspicious/noExplicitAny: it's ok
+  any,
+  // biome-ignore lint/suspicious/noExplicitAny: it's ok
+  any,
+  // biome-ignore lint/suspicious/noExplicitAny: it's ok
+  any,
+  // biome-ignore lint/suspicious/noExplicitAny: it's ok
+  any,
+  // biome-ignore lint/suspicious/noExplicitAny: it's ok
+  any
+>
+
+/**
+ * Handler for POST, PUT and PATCH methods
+ */
+export type FastifyPayloadHandlerFn<ReplyType, BodyType, ParamsType, QueryType, HeadersType> = (
+  req: FastifyRequest<{
+    Body: BodyType
+    Headers: HeadersType
+    Params: ParamsType
+    Querystring: QueryType
+    Reply: ReplyType
+  }>,
+  reply: FastifyReply,
+) => Promise<void>
+
+/**
+ * Handler for GET and DELETE methods
+ */
+export type FastifyNoPayloadHandlerFn<ReplyType, ParamsType, QueryType, HeadersType> = (
+  req: FastifyRequest<{
+    Body: never
+    Headers: HeadersType
+    Params: ParamsType
+    Querystring: QueryType
+    Reply: ReplyType
+  }>,
+  reply: FastifyReply<{ Body: ReplyType }>,
+) => Promise<void>
