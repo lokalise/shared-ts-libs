ad6ecc581056dbeecbdc1a85c20f95195e32c7ad
Anton Malich <anton.malich@lokalise.com>
2025-03-04 15:29:16 +0300

    * adjust removeNonTranslatableTags logic (#513)

--- packages/app/non-translatable-markup/src/methods/removeNonTranslatableTags.spec.ts
+++ packages/app/non-translatable-markup/src/methods/removeNonTranslatableTags.spec.ts
@@ -12,6 +12,8 @@ describe('removeNonTranslatableTags', () => {
     ['{% if MyVariable %}\uE102', '{% if MyVariable %}\uE102'],
     ['{% if MyVariable %}\uE101\uE102', '{% if MyVariable %}\uE101\uE102'],
     ['\uE101\uE101\uE110\uE102 text \uE101\uE102\uE111\uE102', '\uE101\uE110 text \uE102\uE111'],
+    ['\uE101\uE102\uE111\uE101\uE112\uE102', '\uE102\uE111\uE101\uE112'], // duplicated NT tags inside of NT region
+    ['\uE101\uE102\uE111\uE102\uE101\uE101\uE112\uE102', '\uE102\uE111\uE101\uE112'], // duplicated NT tags inside of NT region
   ])('removes non-translatable tags', (originalValue, updatedValue) => {
     expect(removeNonTranslatableTags(originalValue)).toBe(updatedValue)
   })
