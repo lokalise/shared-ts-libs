6a1558981b1a78235d22e2df6a5cf61d28d0d0f5
dosexe <anton.malich@lokalise.com>
2025-03-04 20:21:36 +0800

    * adjust removeNonTranslatableTags logic

--- packages/app/non-translatable-markup/src/methods/utils.ts
+++ packages/app/non-translatable-markup/src/methods/utils.ts
@@ -1,6 +1,6 @@
 import { NON_TRANSLATABLE_END_TAG, NON_TRANSLATABLE_START_TAG } from '../nonTranslatableTags'
 
-const nonTranslatableTextPattern = `${NON_TRANSLATABLE_START_TAG}.*?${NON_TRANSLATABLE_END_TAG}`
+const nonTranslatableTextPattern = `${NON_TRANSLATABLE_START_TAG}.+?${NON_TRANSLATABLE_END_TAG}`
 export const nonTranslatableTextRegexp = new RegExp(nonTranslatableTextPattern)
 export const nonTranslatableTextRegexpG = new RegExp(nonTranslatableTextPattern, 'g')
 
