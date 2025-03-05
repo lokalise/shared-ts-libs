6f58e8e1feb3da18ee69e247a72b623ec14fe9f7
Mateusz Tkacz <mateusz.tkacz@lokalise.com>
2025-03-05 14:34:46 +0100

    chore: exec lint fix

--- packages/app/non-translatable-markup/tsup.config.ts
+++ packages/app/non-translatable-markup/tsup.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'tsup'
 
 // biome-ignore lint/style/noDefaultExport:
 export default defineConfig({
-    entry: ['src/index.ts'],
-    format: ['cjs', 'esm'],
-    outDir: 'dist',
-    clean: true,
-    dts: true,
-    sourcemap: true,
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  outDir: 'dist',
+  clean: true,
+  dts: true,
+  sourcemap: true,
 })
