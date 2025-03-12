a812310a05d1065d4e02b7f81e5e240744b2db3c
CarlosGamero <101278162+CarlosGamero@users.noreply.github.com>
2025-03-12 21:04:34 +0100

    Chore/api common ts config (#533)
    * api-common using common ts config
    
    * import fixes
    
    * build fix
    
    * Lint fix
    
    * Moving tests to src
    
    * Lint fix
    
    * More lint fixes
    
    * coverage fix
    
    * Fixing package.json

--- packages/app/api-common/vite.config.ts
+++ packages/app/api-common/vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'node:path'
 
 import defineConfig from '@lokalise/package-vite-config/package'
+import dtsPlugin from 'vite-plugin-dts'
 
 // @ts-ignore
 import packageJson from './package.json'
@@ -9,9 +10,11 @@ import packageJson from './package.json'
 export default defineConfig({
   entry: resolve(__dirname, 'src/index.ts'),
   dependencies: Object.keys(packageJson.peerDependencies),
+  plugins: [dtsPlugin({ tsconfigPath: './tsconfig.build.json' })],
   test: {
     coverage: {
       provider: 'v8',
+      exclude: ['src/index.ts'],
       thresholds: {
         lines: 100,
         functions: 100,
