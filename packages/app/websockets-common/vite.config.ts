7125f619bdc7b259c3856ae151c9e2e7447ca968
CarlosGamero <carlos.gamero@lokalise.com>
2025-03-13 21:11:46 +0100

    Using just vite

---
+++ packages/app/websockets-common/vite.config.ts
@@ -0,0 +1,20 @@
+import { resolve } from 'node:path'
+
+import defineConfig from '@lokalise/package-vite-config/package'
+
+// @ts-ignore
+import packageJson from './package.json'
+import dtsPlugin from 'vite-plugin-dts'
+
+// biome-ignore lint/style/noDefaultExport: <explanation>
+export default defineConfig({
+  entry: resolve(__dirname, 'src/index.ts'),
+  dependencies: Object.keys(packageJson.dependencies),
+  plugins: [dtsPlugin({ tsconfigPath: './tsconfig.build.json' })],
+  test: {
+    coverage: {
+      provider: 'v8',
+      exclude: ['src/types', 'src/events', 'src/index.ts', 'src/utils/schemaUtils.ts'],
+    },
+  },
+})
