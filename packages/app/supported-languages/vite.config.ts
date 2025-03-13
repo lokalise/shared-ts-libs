bce875d769d23b6e586cbe9891422e787888b744
CarlosGamero <carlos.gamero@lokalise.com>
2025-03-13 20:54:03 +0100

    Increasing coverage

--- packages/app/supported-languages/vite.config.ts
+++ packages/app/supported-languages/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       thresholds: {
         lines: 100,
         functions: 100,
-        branches: 92,
+        branches: 100,
         statements: 100,
       },
     },
