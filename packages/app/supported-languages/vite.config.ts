5baaf97205c675614a92e8d923f90dc854917df5
CarlosGamero <carlos.gamero@lokalise.com>
2025-03-13 20:48:45 +0100

    Switching from rollup to vite

---
+++ packages/app/supported-languages/vite.config.ts
@@ -0,0 +1,21 @@
+import { resolve } from 'node:path'
+import defineConfig from '@lokalise/package-vite-config/package'
+import dtsPlugin from 'vite-plugin-dts'
+
+// biome-ignore lint/style/noDefaultExport: <explanation>
+export default defineConfig({
+  entry: resolve(__dirname, 'src/index.ts'),
+  plugins: [dtsPlugin({ tsconfigPath: './tsconfig.build.json' })],
+  test: {
+    coverage: {
+      provider: 'v8',
+      exclude: ['src/index.ts'],
+      thresholds: {
+        lines: 100,
+        functions: 100,
+        branches: 92,
+        statements: 100,
+      },
+    },
+  },
+})
