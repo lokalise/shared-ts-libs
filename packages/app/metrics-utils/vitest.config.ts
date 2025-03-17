b9993d1617d542654c2e3241155570b4aa24d832
CarlosGamero <carlos.gamero@lokalise.com>
2025-03-17 17:23:31 +0100

    metrics-utils using tsc

---
+++ packages/app/metrics-utils/vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config'
+
+// biome-ignore lint/style/noDefaultExport: vite expects default export
+export default defineConfig({
+  test: {
+    coverage: {
+      exclude: ['src/**/index.ts', 'src/**/*.spec.ts'],
+      provider: 'v8',
+      thresholds: {
+        lines: 99,
+        functions: 99,
+        branches: 84,
+        statements: 99,
+      },
+    },
+  },
+})
