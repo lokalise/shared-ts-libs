e735ad76ada225f0178b4f31ae17295d82cf06a0
CarlosGamero <carlos.gamero@lokalise.com>
2025-03-17 17:17:46 +0100

    api-common using tsc

---
+++ packages/app/api-common/vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config'
+
+// biome-ignore lint/style/noDefaultExport: <explanation>
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'v8',
+      exclude: ['src/index.ts'],
+      thresholds: {
+        lines: 100,
+        functions: 100,
+        branches: 92,
+        statements: 100,
+      },
+    },
+  },
+})
