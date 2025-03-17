287046ab9dfd765e2adde2bcc246844a9fd6da53
CarlosGamero <carlos.gamero@lokalise.com>
2025-03-17 17:25:02 +0100

    supported-languages using tsc

--- packages/app/supported-languages/vitest.config.ts
+++ packages/app/supported-languages/vitest.config.ts
@@ -1,11 +1,7 @@
-import { resolve } from 'node:path'
-import defineConfig from '@lokalise/package-vite-config/package'
-import dtsPlugin from 'vite-plugin-dts'
+import { defineConfig } from 'vitest/config'
 
 // biome-ignore lint/style/noDefaultExport: <explanation>
 export default defineConfig({
-  entry: resolve(__dirname, 'src/index.ts'),
-  plugins: [dtsPlugin({ tsconfigPath: './tsconfig.build.json' })],
   test: {
     coverage: {
       provider: 'v8',
