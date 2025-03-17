36a8476fdf1e00ecba85c8af7f1db5837b954130
CarlosGamero <carlos.gamero@lokalise.com>
2025-03-17 17:13:07 +0100

    Switching to tsc

--- packages/app/frontend-http-client/vitest.config.ts
+++ packages/app/frontend-http-client/vitest.config.ts
@@ -1,15 +1,7 @@
-import { resolve } from 'node:path'
-import defineConfig from '@lokalise/package-vite-config/package'
-import dtsPlugin from 'vite-plugin-dts'
-
-// @ts-ignore
-import packageJson from './package.json'
+import { defineConfig } from 'vitest/config'
 
 // biome-ignore lint/style/noDefaultExport: vite expects a default export
 export default defineConfig({
-  entry: resolve(__dirname, 'src/index.ts'),
-  dependencies: Object.keys(packageJson.peerDependencies),
-  plugins: [dtsPlugin({ tsconfigPath: './tsconfig.build.json' })],
   test: {
     globals: true,
     coverage: {
