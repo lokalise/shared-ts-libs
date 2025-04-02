a46a947637fc297bdc0b0a39dd80c7e835c0fbb2
CarlosGamero <101278162+CarlosGamero@users.noreply.github.com>
2025-04-02 17:41:27 +0200

    Test using bot access token (#576)
    * Test using bot access token
    
    * Triggering api-common release

--- packages/app/api-common/vitest.config.ts
+++ packages/app/api-common/vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       include: ['src/**/*.ts'],
-      exclude: ['src/index.ts'],
+      exclude: ['src/index.ts', 'src/**/*.spec.ts', 'src/**/*.test.ts'],
       thresholds: {
         lines: 100,
         functions: 100,
