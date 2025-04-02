d387e5cdb7d28ceb0f755ce07c417c755b87edcb
Igor Savin <iselwin@gmail.com>
2025-04-03 01:02:31 +0300

    cleanup

--- packages/app/mockttp-utils/vitest.config.ts
+++ packages/app/mockttp-utils/vitest.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vitest/config'
 
-// biome-ignore lint/style/noDefaultExport: <explanation>
+// biome-ignore lint/style/noDefaultExport: vitest expects default export
 export default defineConfig({
   test: {
     coverage: {
