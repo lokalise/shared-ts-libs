5d95a05914396afbfd7525272027a5194d9e6c92
CarlosGamero <carlos.gamero@lokalise.com>
2025-04-23 16:11:50 +0200

    EXP-325 Remvoing barrel file

--- packages/app/aws-config/src/message-queue-toolkit/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './types.ts'
-export * from './MessageQueueToolkitSnsOptionsResolver.ts'
-export type { RequestContextPreHandlerOutput } from './prehandlers/createRequestContextPreHandler.ts'
