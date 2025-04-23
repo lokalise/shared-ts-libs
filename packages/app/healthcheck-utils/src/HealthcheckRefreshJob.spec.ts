7a1521086c0da7fa5e08a11580ab9180ecee8717
Anton Malich <anton.malich@lokalise.com>
2025-04-23 13:59:17 +0300

    * return false on empty healthcheck (#601)
    * * return false on empty healthcheck

--- packages/app/healthcheck-utils/src/HealthcheckRefreshJob.spec.ts
+++ packages/app/healthcheck-utils/src/HealthcheckRefreshJob.spec.ts
@@ -108,14 +108,6 @@ describe('HealthcheckRefreshJob', () => {
         true,
         false,
       ),
-
-      new TestHealthcheck2(
-        {
-          healthcheckStore: store,
-        },
-        true,
-        false,
-      ),
     ]
     job = new HealthcheckRefreshJob(getTestDependencies(), healthchecks)
 
@@ -141,5 +133,42 @@ describe('HealthcheckRefreshJob', () => {
 
     const healthcheckSuccess = store.getHealthcheckResult('test')
     expect(healthcheckSuccess).toBe(false)
+  })
+
+  it('returns failed for unregistered healthchecks', async () => {
+    job = new HealthcheckRefreshJob(getTestDependencies(), [])
+
+    await job.process(randomUUID())
+
+    const healthcheckSuccess = store.getHealthcheckResult('test')
+    expect(healthcheckSuccess).toBe(false)
+  })
+
+  it('updates successfully for multiple healthchecks', async () => {
+    store = new HealthcheckResultsStore({ maxHealthcheckNumber: 2 })
+    const healthchecks = [
+      new TestHealthcheck(
+        {
+          healthcheckStore: store,
+        },
+        true,
+        false,
+      ),
+      new TestHealthcheck2(
+        {
+          healthcheckStore: store,
+        },
+        true,
+        false,
+      ),
+    ]
+    job = new HealthcheckRefreshJob(getTestDependencies(), healthchecks)
+
+    await job.process(randomUUID())
+
+    const healthcheckSuccess1 = store.getHealthcheckResult('test')
+    const healthcheckSuccess2 = store.getHealthcheckResult('test2')
+    expect(healthcheckSuccess1).toBe(true)
+    expect(healthcheckSuccess2).toBe(true)
   })
 })
--- packages/app/healthcheck-utils/src/healthchecks.ts
+++ packages/app/healthcheck-utils/src/healthchecks.ts
@@ -29,7 +29,7 @@ export abstract class AbstractHealthcheck<SupportedHealthchecks extends string>
 
   abstract getId(): SupportedHealthchecks
 
-  constructor(
+  protected constructor(
     dependencies: HealthcheckDependencies<SupportedHealthchecks>,
     areMetricsEnabled: boolean,
   ) {
@@ -52,7 +52,7 @@ export abstract class AbstractHealthcheck<SupportedHealthchecks extends string>
       help: `Whether ${id} was available at the time`,
       collect() {
         const checkResult = store.getHealthcheckResult(id)
-        this.set(checkResult !== false ? 1 : 0)
+        this.set(checkResult ? 1 : 0)
       },
     })
     new Gauge({
