afc10b5ff000fbe4450de2aa60678dd2c472a91e
Igor Savin <iselwin@gmail.com>
2025-04-24 22:42:30 +0300

    feat: Add extra msw test methods (#605)

--- packages/app/universal-testing-utils/src/MswHelper.ts
+++ packages/app/universal-testing-utils/src/MswHelper.ts
@@ -6,7 +6,7 @@ import {
 import type { InferSchemaInput } from '@lokalise/api-contracts'
 import { http, HttpResponse } from 'msw'
 import type { SetupServerApi } from 'msw/node'
-import type { z } from 'zod'
+import type { ZodObject, z } from 'zod'
 
 export type CommonMockParams = {
   responseCode?: number
@@ -61,5 +61,70 @@ export class MswHelper {
         }),
       ),
     )
+  }
+
+  mockValidResponseWithAnyPath<
+    ResponseBodySchema extends z.Schema,
+    PathParamsSchema extends z.Schema | undefined,
+  >(
+    contract: CommonRouteDefinition<
+      InferSchemaOutput<PathParamsSchema>,
+      ResponseBodySchema,
+      PathParamsSchema
+    >,
+    server: SetupServerApi,
+    params: MockParamsNoPath<InferSchemaInput<ResponseBodySchema>>,
+  ): void {
+    const pathParams = contract.requestPathParamsSchema
+      ? Object.keys(
+          // @ts-expect-error this is fine
+          (contract.requestPathParamsSchema as unknown as ZodObject<unknown>).shape,
+        ).reduce(
+          (acc, value) => {
+            acc[value] = '*'
+            return acc
+          },
+          {} as Record<string, string>,
+        )
+      : {}
+
+    const path = contract.requestPathParamsSchema
+      ? // @ts-expect-error this is safe
+        contract.pathResolver(pathParams)
+      : mapRouteToPath(contract)
+
+    const resolvedPath = joinURL(this.baseUrl, path)
+    // @ts-expect-error
+    const method: 'get' | 'delete' | 'post' | 'patch' | 'put' = contract.method
+    server.use(
+      http[method](resolvedPath, () =>
+        HttpResponse.json(params.responseBody, {
+          status: params.responseCode,
+        }),
+      ),
+    )
+  }
+
+  mockAnyResponse<PathParamsSchema extends z.Schema | undefined>(
+    contract: CommonRouteDefinition<
+      InferSchemaOutput<PathParamsSchema>,
+      // biome-ignore lint/suspicious/noExplicitAny: we accept any input
+      any,
+      PathParamsSchema
+    >,
+    server: SetupServerApi,
+    params: PathParamsSchema extends undefined
+      ? MockParamsNoPath<
+          // biome-ignore lint/suspicious/noExplicitAny: we accept any input
+          InferSchemaInput<any>
+        >
+      : MockParams<
+          InferSchemaInput<PathParamsSchema>,
+          // biome-ignore lint/suspicious/noExplicitAny: we accept any input
+          InferSchemaInput<any>
+        >,
+  ): void {
+    // biome-ignore lint/suspicious/noExplicitAny: we accept any input
+    this.mockValidResponse<any, PathParamsSchema>(contract, server, params)
   }
 }
