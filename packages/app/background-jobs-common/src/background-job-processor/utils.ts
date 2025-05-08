6df531073feb4f6e9062a70d9a30a6f60519322a
CarlosGamero <carlos.gamero@lokalise.com>
2025-05-08 20:20:05 +0200

    Fixing tests

--- packages/app/background-jobs-common/src/background-job-processor/utils.ts
+++ packages/app/background-jobs-common/src/background-job-processor/utils.ts
@@ -2,19 +2,39 @@ import { generateMonotonicUuid } from '@lokalise/id-utils'
 import type { RedisConfig } from '@lokalise/node-core'
 import type { JobsOptions } from 'bullmq'
 import type { Redis } from 'ioredis'
-import { DEFAULT_JOB_CONFIG } from './constants.ts'
 import type { QueueConfiguration } from './managers/index.ts'
 import type { BackgroundJobProcessorConfig } from './processors/types.ts'
 import { QUEUE_GROUP_DELIMITER } from './public-utils/index.ts'
 import type { SafeJob } from './types.ts'
+import {
+  RETENTION_COMPLETED_JOBS_IN_AMOUNT,
+  RETENTION_COMPLETED_JOBS_IN_DAYS,
+  RETENTION_FAILED_JOBS_IN_DAYS,
+} from './constants.ts'
 
-export const daysToSeconds = (days: number): number => days * 24 * 60 * 60
+const daysToSeconds = (days: number): number => days * 24 * 60 * 60
 
 export const daysToMilliseconds = (days: number): number => daysToSeconds(days) * 1000
 
 export const isRedisClient = (redis: RedisConfig | Redis): redis is Redis => 'options' in redis
 
 export const resolveJobId = (job?: SafeJob<unknown>): string => job?.id ?? 'unknown'
+
+/**
+ * Default config
+ *    - Retry config: 3 retries with 30s of total amount of wait time between retries using
+ *            exponential strategy https://docs.bullmq.io/guide/retrying-failing-jobs#built-in-backoff-strategies
+ *    - Job retention: 50 last completed jobs, 7 days for failed jobs
+ */
+const DEFAULT_JOB_CONFIG: JobsOptions = {
+  attempts: 3,
+  backoff: { type: 'exponential', delay: 5000 },
+  removeOnComplete: {
+    count: RETENTION_COMPLETED_JOBS_IN_AMOUNT,
+    age: daysToSeconds(RETENTION_COMPLETED_JOBS_IN_DAYS),
+  },
+  removeOnFail: { age: daysToSeconds(RETENTION_FAILED_JOBS_IN_DAYS) },
+}
 
 export const prepareJobOptions = <JobOptionsType extends JobsOptions>(
   isTest: boolean,
