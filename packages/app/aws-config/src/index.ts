51149835addd644d1319fd6e50f0570ff2c3f0cd
Julio Montoya <gugli100@gmail.com>
2025-06-25 17:07:20 +0200

    Updates app/aws-config

--- packages/app/aws-config/src/index.ts
+++ packages/app/aws-config/src/index.ts
@@ -1,17 +1,17 @@
+export * from './applyAwsResourcePrefix.ts'
+export { type AwsConfig, getAwsConfig } from './awsConfig.ts'
 export type {
+  EventRoutingConfig,
   QueueConfig,
   TopicConfig,
-  EventRoutingConfig,
 } from './event-routing/eventRoutingConfig.ts'
-export type {
-  MessageQueueToolkitSnsOptionsResolverConfig,
-  ResolvePublisherBuildOptionsParams,
-  ResolveConsumerBuildOptionsParams,
-  ResolvedSnsPublisherBuildOptions,
-  ResolvedSnsConsumerBuildOptions,
-} from './message-queue-toolkit/types.ts'
 export { MessageQueueToolkitSnsOptionsResolver } from './message-queue-toolkit/MessageQueueToolkitSnsOptionsResolver.ts'
 export type { RequestContextPreHandlerOutput } from './message-queue-toolkit/prehandlers/createRequestContextPreHandler.ts'
+export type {
+  MessageQueueToolkitSnsOptionsResolverConfig,
+  ResolveConsumerBuildOptionsParams,
+  ResolvedSnsConsumerBuildOptions,
+  ResolvedSnsPublisherBuildOptions,
+  ResolvePublisherBuildOptionsParams,
+} from './message-queue-toolkit/types.ts'
 export * from './tags/index.ts'
-export { getAwsConfig, type AwsConfig } from './awsConfig.ts'
-export * from './applyAwsResourcePrefix.ts'
