4eb730a0727e8eb100de1d5bb209806ebe62c011
Bartosz Drozd <bartosz.drozd@lokalise.com>
2025-07-02 17:11:56 +0200

    refactor: replace cause with details

--- packages/app/background-jobs-common/src/background-job-processor/errors/MutedUnrecoverableError.ts
+++ packages/app/background-jobs-common/src/background-job-processor/errors/MutedUnrecoverableError.ts
@@ -3,9 +3,12 @@ import { UnrecoverableError } from 'bullmq'
 export const MUTED_UNRECOVERABLE_ERROR_SYMBOL = Symbol.for('MUTED_UNRECOVERABLE_ERROR_KEY')
 
 export class MutedUnrecoverableError extends UnrecoverableError {
-  constructor(message?: string, public readonly cause?: unknown) {
+  public readonly details?: Record<string, unknown>
+
+  constructor(message?: string, details?: Record<string, unknown>) {
     super(message)
     this.name = UnrecoverableError.name
+    this.details = details
   }
 }
 
