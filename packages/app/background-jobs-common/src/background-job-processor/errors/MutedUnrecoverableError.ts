c1931dde89705345d5c760ec5c05dd9249ae2e95
Bartosz Drozd <bartosz.drozd@lokalise.com>
2025-07-02 17:05:25 +0200

    feat: add cause to MutedUnrecoverableError

--- packages/app/background-jobs-common/src/background-job-processor/errors/MutedUnrecoverableError.ts
+++ packages/app/background-jobs-common/src/background-job-processor/errors/MutedUnrecoverableError.ts
@@ -3,7 +3,7 @@ import { UnrecoverableError } from 'bullmq'
 export const MUTED_UNRECOVERABLE_ERROR_SYMBOL = Symbol.for('MUTED_UNRECOVERABLE_ERROR_KEY')
 
 export class MutedUnrecoverableError extends UnrecoverableError {
-  constructor(message?: string) {
+  constructor(message?: string, public readonly cause?: unknown) {
     super(message)
     this.name = UnrecoverableError.name
   }
