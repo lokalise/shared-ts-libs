2e4b8590e135d42fa2ba99c8efdb45556f2ebb2a
CarlosGamero <carlos.gamero@lokalise.com>
2025-07-15 12:39:45 +0200

    Adding abstract metric

---
+++ packages/app/metrics-utils/src/AbstractMetric.ts
@@ -0,0 +1,31 @@
+import type promClient from 'prom-client'
+import type { Metric } from 'prom-client'
+
+type BaseMetricParams = {
+  name: string
+}
+
+export abstract class AbstractMetric<
+  MetricType extends Metric,
+  MetricsParams extends BaseMetricParams,
+> {
+  protected readonly metric?: MetricType
+  protected readonly metricConfig: MetricsParams
+
+  protected constructor(metricConfig: MetricsParams, client?: typeof promClient) {
+    this.metricConfig = metricConfig
+
+    if (!client) return
+    this.metric = this.registerMetric(client)
+  }
+
+  private registerMetric(client: typeof promClient): MetricType {
+    const existingMetric = client.register.getSingleMetric(this.metricConfig.name)
+
+    return existingMetric ? (existingMetric as MetricType) : this.createMetric(client)
+  }
+
+  protected abstract createMetric(client: typeof promClient): MetricType
+
+  public abstract registerMeasurement(measurement: unknown): void
+}
