3ed8828e3ab6bb0949ddc3c1d29d31271fcc45e4
Nic Robinson <nicholas.robinson@lokalise.com>
2025-07-18 12:23:21 +0100

    fix: better error reporting in the case of never not being provided and related tests

--- packages/app/universal-ts-utils/src/public/type/assertIsNever.test.ts
+++ packages/app/universal-ts-utils/src/public/type/assertIsNever.test.ts
@@ -5,4 +5,24 @@ describe('assertIsNever', () => {
   it('should throw an error if a non-never value is passed in', () => {
     expect(() => assertIsNever('not-never' as never)).toThrowError()
   })
+
+  describe('assertIsNever', () => {
+    it('should throw an error if a non-never value is passed in', () => {
+      expect(() => assertIsNever('not-never' as never)).toThrowError()
+    })
+
+    it('should throw an error with descriptive message for string values', () => {
+      expect(() => assertIsNever('test' as never)).toThrowError('Unexpected value: "test"')
+    })
+
+    it('should throw an error with descriptive message for object values', () => {
+      expect(() => assertIsNever({ key: 'value' } as never)).toThrowError(
+        'Unexpected value: {"key":"value"}',
+      )
+    })
+
+    it('should throw an error with descriptive message for null values', () => {
+      expect(() => assertIsNever(null as never)).toThrowError('Unexpected value: null')
+    })
+  })
 })
