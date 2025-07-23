ce2047e6cc523c8dc7cab270455ccf61f3add458
Nic Robinson <nicholas.robinson@lokalise.com>
2025-07-23 10:07:25 +0100

    chore: more descriptive jsdocs

--- packages/app/universal-ts-utils/src/public/type/assertIsNever.ts
+++ packages/app/universal-ts-utils/src/public/type/assertIsNever.ts
@@ -1,5 +1,9 @@
 /**
  * Asserts that the provided value is of type `never` and will cause a TypeScript error if it is not.
+ * The goal of this is to error at build time if there is a case that is not covered.
+ * There is a fallback for runtime is something sneaks though. We are not trying to determine if something
+ * is `never` (i.e. `(value: unknown): value is never => {...}`) – we are, instead, trying to catch the case
+ * that something is not `never` when we expect it to be. In reality, this should ALWAYS be unreachable code.
  *
  * This is useful for exhaustive type checking in switch statements or conditional types,
  * ensuring that all possible cases have been handled.
