60cac116d725244f65e290db117bdd843fb3164b
CarlosGamero <101278162+CarlosGamero@users.noreply.github.com>
2025-08-13 23:04:59 +0200

    chore: Biome 2 (#717)
    * Deps update
    
    * Fixing devs deps
    
    * Biome json migrated
    
    * Lint fix

--- packages/app/supported-languages/vitest.config.ts
+++ packages/app/supported-languages/vitest.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vitest/config'
 
-// biome-ignore lint/style/noDefaultExport: <explanation>
+// biome-ignore lint/style/noDefaultExport: Expected
 export default defineConfig({
   test: {
     globals: true,
