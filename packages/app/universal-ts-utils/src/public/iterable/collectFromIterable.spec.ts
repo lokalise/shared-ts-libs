bce02ec3d904d4871e193f8aa87d73129dddc23b
CarlosGamero <carlos.gamero@lokalise.com>
2025-09-18 12:26:41 +0200

    Adding type test

--- packages/app/universal-ts-utils/src/public/iterable/collectFromIterable.spec.ts
+++ packages/app/universal-ts-utils/src/public/iterable/collectFromIterable.spec.ts
@@ -1,5 +1,5 @@
 import { setTimeout } from 'node:timers/promises'
-import { describe, expect, it } from 'vitest'
+import { describe, expect, expectTypeOf, it } from 'vitest'
 import { collectFromIterable } from './collectFromIterable.ts'
 
 describe('collectFromIterable', () => {
@@ -41,24 +41,33 @@ describe('collectFromIterable', () => {
       const result = collectFromIterable(mixedArray)
       expect(result).toEqual(['hello', 42, true, null, undefined, { hello: 'world' }])
     })
-  })
 
-  describe('async iterables', () => {
-    it('collects items from an async generator', async () => {
-      async function* asyncGeneratorWithDelay() {
-        yield await Promise.resolve(1)
-        await setTimeout(5)
-        yield await Promise.resolve(2)
-        await setTimeout(5)
-        yield await Promise.resolve(3)
-        await setTimeout(5)
-      }
-      const result = await collectFromIterable(asyncGeneratorWithDelay())
-      expect(result).toEqual([1, 2, 3])
+    it('should infer return type properly', () => {
+      const result = collectFromIterable([1, 2, 3])
+      expectTypeOf(result).toEqualTypeOf<number[]>()
+      expectTypeOf(result).not.toEqualTypeOf<Promise<number[]>>()
     })
   })
 
-  it('infer return type depending on input param', () => {
-    // TODO
+  describe('async iterables', () => {
+    async function* asyncGenerator() {
+      yield await Promise.resolve(1)
+      await setTimeout(5)
+      yield await Promise.resolve(2)
+      await setTimeout(5)
+      yield await Promise.resolve(3)
+      await setTimeout(5)
+    }
+
+    it('collects items from an async generator', async () => {
+      const result = await collectFromIterable(asyncGenerator())
+      expect(result).toEqual([1, 2, 3])
+    })
+
+    it('should infer return type properly', () => {
+      const result = collectFromIterable(asyncGenerator())
+      expectTypeOf(result).toEqualTypeOf<Promise<number[]>>()
+      expectTypeOf(result).not.toEqualTypeOf<number[]>()
+    })
   })
 })
