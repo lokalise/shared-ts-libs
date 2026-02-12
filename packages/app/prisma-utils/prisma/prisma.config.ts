072ae76840789468c42c56940946b39bec0fc0a5
CarlosGamero <carlos.gamero@lokalise.com>
2026-02-12 17:15:00 +0100

    Adjusting client generation for prisma 7

---
+++ packages/app/prisma-utils/prisma/prisma.config.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config'
+
+export default defineConfig({
+  datasource: { url: env('DATABASE_URL') },
+  schema: './schema.prisma',
+  migrations: {
+    path: './migrations',
+  },
+})
