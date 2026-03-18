c5a1ee42b5a18818b4f65f6ab0e46df074253b34
CarlosGamero <carlos.gamero@lokalise.com>
2026-03-18 18:47:34 +0100

    index fix

--- packages/app/supported-languages/src/constants/index.ts
+++ packages/app/supported-languages/src/constants/index.ts
@@ -1,6 +1,5 @@
 // Intentionally, we only export types. Constants have their own getters
-export type * from './languages.ts'
-export type * from './lokaliseLanguages.ts'
-export type * from './regions.ts'
-export type * from './scripts.ts'
-export type * from './standard-locales.ts'
+export type { Language } from './languages.ts'
+export type { Region } from './regions.ts'
+export type { Script } from './scripts.ts'
+export type { StandardLocale } from './standard-locales.ts'
