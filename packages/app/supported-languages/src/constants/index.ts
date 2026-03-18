d1b0d64a45a1d6c296451a8636d73deb9464721c
CarlosGamero <carlos.gamero@lokalise.com>
2026-03-18 18:05:14 +0100

    Improving constants + export

---
+++ packages/app/supported-languages/src/constants/index.ts
@@ -0,0 +1,6 @@
+// Intentionally, we only export types. Constants have their own getters
+export type * from './languages.ts'
+export type * from './lokaliseLanguages.ts'
+export type * from './regions.ts'
+export type * from './scripts.ts'
+export type * from './standard-locales.ts'
