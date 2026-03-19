7bd6c4d6b4ae6e32209bc842bbea64fd08a1f861
CarlosGamero <carlos.gamero@lokalise.com>
2026-03-19 21:23:54 +0100

    typo

--- packages/app/supported-languages/src/intl.d.ts
+++ packages/app/supported-languages/src/intl.d.ts
@@ -1,6 +1,6 @@
 declare namespace Intl {
   interface Locale {
-    getTextInfo?: () => { direction: 'ltr' | 'rtl' } // Available on Node >= 24
-    textInfo?: { direction: 'ltr' | 'rtl' } // Available on Node >= 20
+    getTextInfo?: () => { direction: 'ltr' | 'rtl' } // Available on Node 24+
+    textInfo?: { direction: 'ltr' | 'rtl' } // Available on Node 20+
   }
 }
