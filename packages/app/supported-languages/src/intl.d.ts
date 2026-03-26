3b836693a9cc8d13224edeae84b7a18a7486003e
CarlosGamero <carlos.gamero@lokalise.com>
2026-03-26 18:15:02 +0100

    Removing unused type

--- packages/app/supported-languages/src/intl.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace Intl {
-  interface Locale {
-    getTextInfo?: () => { direction: 'ltr' | 'rtl' } // Available on Node 24+
-    textInfo?: { direction: 'ltr' | 'rtl' } // Available on Node 20+
-  }
-}
