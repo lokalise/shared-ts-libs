e7a8bad9a514ac62055fc29d4e7bc28ea4181348
Igor Savin <igor.savin@lokalise.com>
2026-03-26 13:08:01 +0200

    fix tests

--- packages/app/drizzle-utils/test/getMysqlDatabaseUrl.ts
+++ packages/app/drizzle-utils/test/getMysqlDatabaseUrl.ts
@@ -1,7 +1,7 @@
 const MYSQL_DATABASE_URL_ENVVAR = 'MYSQL_DATABASE_URL'
 
 export const getMysqlDatabaseUrl = (): string => {
-  const databaseUrl = process.env[MYSQL_DATABASE_URL_ENVVAR]
+  const databaseUrl = (process.env[MYSQL_DATABASE_URL_ENVVAR] ?? '').trim()
 
   if (!databaseUrl) {
     throw new Error(`Environment variable ${MYSQL_DATABASE_URL_ENVVAR} is not set`)
