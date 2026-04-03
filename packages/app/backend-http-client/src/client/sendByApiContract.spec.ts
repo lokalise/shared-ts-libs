b0d3a172fbaaed97a33151efe7ccfd7b73eaebcb
Mateusz Tkacz <mateusz.tkacz@lokalise.com>
2026-04-03 15:06:18 +0200

    fix: failing test

--- packages/app/backend-http-client/src/client/sendByApiContract.spec.ts
+++ packages/app/backend-http-client/src/client/sendByApiContract.spec.ts
@@ -563,7 +563,7 @@ describe('sendByApiContract', () => {
 
       expect(callCount).toBe(2)
       expect(result.error).toMatchObject({
-        message: expect.stringContaining('Could not map response'),
+        message: 'Failed to process API response. (Status: 503, Content-Type: application/json)'
       })
       expect(result.result).toBeUndefined()
     })
