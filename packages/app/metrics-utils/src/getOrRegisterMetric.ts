e1d9f5760b212e75a45bbbbffde0c433bda25bd9
CarlosGamero <carlos.gamero@lokalise.com>
2026-04-22 16:52:51 +0200

    Refactoring abstract metric

---
+++ packages/app/metrics-utils/src/getOrRegisterMetric.ts
@@ -0,0 +1,11 @@
+import type promClient from 'prom-client'
+import type { Metric } from 'prom-client'
+
+export const getOrRegisterMetric = <T extends Metric>(
+  client: typeof promClient,
+  name: string,
+  factory: () => T,
+): T => {
+  const existing = client.register.getSingleMetric(name)
+  return existing ? (existing as T) : factory()
+}
