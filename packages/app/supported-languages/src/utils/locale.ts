553de90ecfc92ce194fd370e54545e1fcea92d66
CarlosGamero <carlos.gamero@lokalise.com>
2026-07-15 14:15:29 +0200

    Minor change

--- packages/app/supported-languages/src/utils/locale.ts
+++ packages/app/supported-languages/src/utils/locale.ts
@@ -63,9 +63,7 @@ export const stringifyLocale = (obj: LocaleObject): Locale =>
  * @throws {RangeError} If locale is structurally invalid or values are not in our supported values
  */
 export const parseLocale = (tag: Locale): Either<string, LocaleObject> => {
-  if (!isSupportedLocale(tag)) {
-    return { error: `Locale tag ${tag} is not supported` }
-  }
+  if (!isSupportedLocale(tag)) return { error: `Locale tag ${tag} is not supported` }
 
   const { language, script, region } = new Intl.Locale(tag)
 
