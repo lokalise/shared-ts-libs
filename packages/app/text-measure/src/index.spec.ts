3a0549aa3f51fcdf9d302f1b1f1cd985d0c16ba7
CarlosGamero <carlos.gamero@lokalise.com>
2026-07-29 18:37:48 +0200

    lint

--- packages/app/text-measure/src/index.spec.ts
+++ packages/app/text-measure/src/index.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { hello } from './index.js'
+import { hello } from './index.ts'
 
 describe('text-measure', () => {
   it('exports hello', () => {
